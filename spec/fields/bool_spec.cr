0d2b92f6004084980c0a50b4f750106658a9c158
kebasyaty <kebasyaty@gmail.com>
2023-10-17 12:38:29 +0300

    Update test for Boolean field

--- spec/fields/bool_spec.cr
+++ spec/fields/bool_spec.cr
@@ -6,6 +6,11 @@ describe Fields do
       it "create an instance of the Boolean field" do
         f = Fields::Boolean.new
         f.id.should eq("")
+        f.label.should eq("")
+        f.field_type.should eq("Boolean")
+        f.input_type.should eq("checkbox")
+        f.name.should eq("")
+        f.value.should be_nil
       end
     end
   end
--- src/fields/field.cr
+++ src/fields/field.cr
@@ -3,5 +3,10 @@
 module Fields
   abstract class Field
     property id : String = ""
+    property label : String = ""
+    property field_type : String = "Boolean"
+    property input_type : String = "checkbox"
+    property name : String = ""
+    property value : Bool | Nil
   end
 end
