b1f7f33a57f80afbe0da66fd5475eeff998a8430
kebasyaty <kebasyaty@gmail.com>
2023-10-18 08:50:36 +0300

    Add test for ChoiceTextField

---
+++ spec/fields/choice_text_spec.cr
@@ -0,0 +1,30 @@
+require "../spec_helper"
+
+describe Fields do
+  describe "Fields::ChoiceTextField" do
+    describe ".new" do
+      it "create an instance of the ChoiceText field" do
+        f = Fields::TextField.new
+        f.id.should eq("")
+        f.label.should eq("")
+        f.field_type.should eq("ChoiceTextField")
+        f.name.should eq("")
+        f.value.should be_nil
+        f.default.should be_nil
+        f.placeholder.should eq("")
+        f.is_required.should be_false
+        f.is_unique.should be_false
+        f.is_disabled.should be_false
+        f.is_readonly.should be_false
+        f.is_hide.should be_false
+        f.choices.should eq(Array(Tuple(String, String)).new)
+        f.other_attrs.should eq("")
+        f.css_classes.should eq("")
+        f.hint.should eq("")
+        f.warning.should eq("")
+        f.errors.should eq(Array(String).new)
+        f.group.should eq(4_u32)
+      end
+    end
+  end
+end
--- src/fields/choice_text.cr
+++ src/fields/choice_text.cr
@@ -12,10 +12,8 @@ module Fields
     property default : String | Nil
     # Specifies that multiple options can be selected at once.
     getter multiple : Bool = false
-    # The minimum number of characters allowed in the text.
-    property minlength : UInt32 = 0
-    # The maximum number of characters allowed in the text.
-    property maxlength : UInt32 = 256
+    # Displays prompt text.
+    property placeholder : String = ""
     # The unique value of a field in a collection.
     property is_unique : Bool = false
     #
