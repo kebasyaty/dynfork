a38fdcdf868a5604488928b93dd49979361a36bf
kebasyaty <kebasyaty@gmail.com>
2023-10-18 08:40:51 +0300

    Add ChoiceTextField

--- src/fields/choice_text.cr
+++ src/fields/choice_text.cr
@@ -1,28 +1,27 @@
 require "./field"
 
 module Fields
-  # A field for entering a text string.
-  class TextField < Fields::Field
+  # Type of selective field with static of elements.
+  # With a single choice.
+  class ChoiceTextField < Fields::Field
     # Field type - Class Name.
-    getter field_type : String = "TextField"
-    # Field type - Html, input type.
-    getter input_type : String = "text"
-    # For Html textarea.
-    property is_textarea : Bool = false
+    getter field_type : String = "ChoiceTextField"
     # Sets the value of an element.
     property value : String | Nil
     # Value by default.
     property default : String | Nil
-    # Displays prompt text.
-    property placeholder : String = ""
+    # Specifies that multiple options can be selected at once.
+    getter multiple : Bool = false
     # The minimum number of characters allowed in the text.
     property minlength : UInt32 = 0
     # The maximum number of characters allowed in the text.
     property maxlength : UInt32 = 256
     # The unique value of a field in a collection.
     property is_unique : Bool = false
+    #
+    choices : Array(Tuple(String, String)) = Array(Tuple(String, String)).new
     # To optimize field traversal in the `paladins/check()` method.
     # Hint: It is recommended not to change.
-    getter group : UInt32 = 1
+    getter group : UInt32 = 4
   end
 end
--- src/fields/url.cr
+++ src/fields/url.cr
@@ -11,12 +11,12 @@ module Fields
     property value : String | Nil
     # Value by default.
     property default : String | Nil
+    # Displays prompt text.
+    property placeholder : String = ""
     # The minimum number of characters allowed in the text.
     property minlength : UInt32 = 0
     # The maximum number of characters allowed in the text.
     property maxlength : UInt32 = 256
-    # Mandatory field.
-    property is_required : Bool = false
     # The unique value of a field in a collection.
     property is_unique : Bool = false
     # To optimize field traversal in the `paladins/check()` method.
