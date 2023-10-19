830ad0bea4d6556b6dd0ce170aa0a5eba886c235
kebasyaty <kebasyaty@gmail.com>
2023-10-19 12:01:53 +0300

    update

--- src/fields/url.cr
+++ src/fields/url.cr
@@ -12,15 +12,32 @@ module Fields
     # Value by default.
     property default : String | Nil
     # Displays prompt text.
-    property placeholder : String = ""
+    property placeholder : String
     # The maximum number of characters allowed in the text.
-    property maxlength : UInt32 = 2048
+    property maxlength : UInt32
     # The minimum number of characters allowed in the text.
-    property minlength : UInt32 = 0
+    property minlength : UInt32
     # The unique value of a field in a collection.
-    property is_unique : Bool = false
+    property is_unique : Bool
     # To optimize field traversal in the `paladins/check()` method.
     # Hint: It is recommended not to change.
     getter group : UInt32 = 1
+
+    def initialize(
+      @label : String = "",
+      @default : String | Nil = nil,
+      @placeholder : String = "",
+      @maxlength : UInt32 = 2048,
+      @minlength : UInt32 = 0,
+      @is_hide : Bool = false,
+      @is_unique : Bool = false,
+      @is_required : Bool = false,
+      @is_disabled : Bool = false,
+      @is_readonly : Bool = false,
+      @other_attrs : String = "",
+      @css_classes : String = "",
+      @hint : String = "",
+      @warning : String = ""
+    ); end
   end
 end
