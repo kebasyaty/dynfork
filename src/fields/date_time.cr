e8dead99bcc31efd1aecc5f07765d5a057137211
kebasyaty <kebasyaty@gmail.com>
2023-10-19 12:07:30 +0300

    Update

--- src/fields/date_time.cr
+++ src/fields/date_time.cr
@@ -15,17 +15,33 @@ module Fields
     # Example: 1970-01-01T00:00
     property default : String | Nil
     # Displays prompt text.
-    property placeholder : String = ""
+    property placeholder : String
     # The top value for entering a date and time.
-    property max : String = ""
+    property max : String
     # The lower value for entering a date and time.
-    property min : String = ""
+    property min : String
     # The unique value of a field in a collection.
-    property is_unique : Bool = false
+    property is_unique : Bool
     # Additional explanation for the user.
-    property hint : String = "Format: yyyy-mm-ddThh:mm"
+    property hint : String
     # To optimize field traversal in the `paladins/check()` method.
     # Hint: It is recommended not to change.
     getter group : UInt32 = 3
+
+    def initialize(
+      @label : String = "",
+      @default : String | Nil = nil,
+      @placeholder : String = "",
+      @max : String = "",
+      @min : String = "",
+      @is_hide : Bool = false,
+      @is_unique : Bool = false,
+      @is_required : Bool = false,
+      @is_disabled : Bool = false,
+      @is_readonly : Bool = false,
+      @other_attrs : String = "",
+      @css_classes : String = "",
+      @hint : String = "Format: yyyy-mm-ddThh:mm"
+    ); end
   end
 end
