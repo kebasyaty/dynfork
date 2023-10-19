0b82d1f31257f4aee2a434e9599094a4b2409ddf
kebasyaty <kebasyaty@gmail.com>
2023-10-19 12:24:26 +0300

    update

--- src/fields/choice_u32.cr
+++ src/fields/choice_u32.cr
@@ -13,13 +13,26 @@ module Fields
     # Specifies that multiple options can be selected at once.
     getter is_multiple : Bool = false
     # The unique value of a field in a collection.
-    property is_unique : Bool = false
+    property is_unique : Bool
     # Example: [{5, "Title"}, {10, "Title 2"}].
     # Html tag: select
     property choices : Array(Tuple(UInt32, String)) = Array(Tuple(UInt32, String)).new
     # To optimize field traversal in the `paladins/check()` method.
     # Hint: It is recommended not to change.
     getter group : UInt32 = 4
+
+    def initialize(
+      @label : String = "",
+      @is_hide : Bool = false,
+      @is_unique : Bool = false,
+      @is_required : Bool = false,
+      @is_disabled : Bool = false,
+      @is_readonly : Bool = false,
+      @other_attrs : String = "",
+      @css_classes : String = "",
+      @hint : String = "",
+      @choices : Array(Tuple(UInt32, String)) = Array(Tuple(UInt32, String)).new
+    ); end
   end
 
   # Type of selective field with static of elements.
@@ -34,13 +47,26 @@ module Fields
     # Specifies that multiple options can be selected at once.
     getter is_multiple : Bool = true
     # The unique value of a field in a collection.
-    property is_unique : Bool = false
+    property is_unique : Bool
     # Example: [{5, "Title"}, {10, "Title 2"}].
     # Html tag: select multiple
     property choices : Array(Tuple(UInt32, String)) = Array(Tuple(UInt32, String)).new
     # To optimize field traversal in the `paladins/check()` method.
     # Hint: It is recommended not to change.
     getter group : UInt32 = 6
+
+    def initialize(
+      @label : String = "",
+      @is_hide : Bool = false,
+      @is_unique : Bool = false,
+      @is_required : Bool = false,
+      @is_disabled : Bool = false,
+      @is_readonly : Bool = false,
+      @other_attrs : String = "",
+      @css_classes : String = "",
+      @hint : String = "",
+      @choices : Array(Tuple(UInt32, String)) = Array(Tuple(UInt32, String)).new
+    ); end
   end
 
   # Type of selective field with dynamic addition of elements.
@@ -54,13 +80,26 @@ module Fields
     # Specifies that multiple options can be selected at once.
     getter is_multiple : Bool = false
     # The unique value of a field in a collection.
-    property is_unique : Bool = false
+    property is_unique : Bool
     # Example: [{5, "Title"}, {10, "Title 2"}].
     # Html tag: select
     property choices : Array(Tuple(UInt32, String)) = Array(Tuple(UInt32, String)).new
     # To optimize field traversal in the `paladins/check()` method.
     # Hint: It is recommended not to change.
     getter group : UInt32 = 5
+
+    def initialize(
+      @label : String = "",
+      @is_hide : Bool = false,
+      @is_unique : Bool = false,
+      @is_required : Bool = false,
+      @is_disabled : Bool = false,
+      @is_readonly : Bool = false,
+      @other_attrs : String = "",
+      @css_classes : String = "",
+      @hint : String = "",
+      @choices : Array(Tuple(UInt32, String)) = Array(Tuple(UInt32, String)).new
+    ); end
   end
 
   # Type of selective field with dynamic addition of elements.
@@ -74,12 +113,25 @@ module Fields
     # Specifies that multiple options can be selected at once.
     getter is_multiple : Bool = true
     # The unique value of a field in a collection.
-    property is_unique : Bool = false
+    property is_unique : Bool
     # Example: [{5, "Title"}, {10, "Title 2"}].
     # Html tag: select
-    property choices : Array(Tuple(UInt32, String)) = Array(Tuple(UInt32, String)).new
+    property choices : Array(Tuple(UInt32, String))
     # To optimize field traversal in the `paladins/check()` method.
     # Hint: It is recommended not to change.
     getter group : UInt32 = 7
+
+    def initialize(
+      @label : String = "",
+      @is_hide : Bool = false,
+      @is_unique : Bool = false,
+      @is_required : Bool = false,
+      @is_disabled : Bool = false,
+      @is_readonly : Bool = false,
+      @other_attrs : String = "",
+      @css_classes : String = "",
+      @hint : String = "",
+      @choices : Array(Tuple(UInt32, String)) = Array(Tuple(UInt32, String)).new
+    ); end
   end
 end
