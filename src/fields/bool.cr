35815b878ea9e5cfaadf88635388e63a8aeead45
kebasyaty <kebasyaty@gmail.com>
2023-10-19 12:53:32 +0300

    update

--- src/fields/bool.cr
+++ src/fields/bool.cr
@@ -10,9 +10,21 @@ module Fields
     # Sets the value of an element.
     property value : Bool | Nil
     # Value by default.
-    property default : Bool | Nil = false
+    property default : Bool | Nil
     # To optimize field traversal in the `paladins/check()` method.
     # Hint: It is recommended not to change.
     getter group : UInt32 = 13
+
+    def initialize(
+      @label : String = "",
+      @default : Bool | Nil = false,
+      @is_hide : Bool = false,
+      @is_required : Bool = false,
+      @is_disabled : Bool = false,
+      @is_readonly : Bool = false,
+      @other_attrs : String = "",
+      @css_classes : String = "",
+      @hint : String = ""
+    ); end
   end
 end
