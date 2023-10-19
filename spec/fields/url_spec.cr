4df720bffe8c93345dd3ebbdbf32289edb962ba0
kebasyaty <kebasyaty@gmail.com>
2023-10-19 09:58:51 +0300

    Update test for URLField

--- spec/fields/url_spec.cr
+++ spec/fields/url_spec.cr
@@ -18,7 +18,7 @@ describe Fields do
         f.is_hide.should be_false
         f.is_required.should be_false
         f.is_unique.should be_false
-        f.maxlength.should eq(256)
+        f.maxlength.should eq(2048)
         f.minlength.should eq(0)
         f.other_attrs.should eq("")
         f.css_classes.should eq("")
