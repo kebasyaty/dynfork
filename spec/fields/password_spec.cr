b7a34f566bf9a4bb6b92d17b47c4559b290df898
kebasyaty <kebasyaty@gmail.com>
2023-10-19 17:30:54 +0300

    Add Password field

---
+++ spec/fields/password_spec.cr
@@ -0,0 +1,33 @@
+require "../spec_helper"
+
+describe Fields do
+  describe "Fields::PasswordField" do
+    describe ".new" do
+      it "create an instance of the Password field" do
+        f = Fields::PasswordField.new
+        f.id.should eq("")
+        f.label.should eq("")
+        f.field_type.should eq("PasswordField")
+        f.input_type.should eq("password")
+        f.name.should eq("")
+        f.value.should be_nil
+        f.placeholder.should eq("")
+        f.is_disabled.should be_false
+        f.is_readonly.should be_false
+        f.is_hide.should be_false
+        f.is_required.should be_false
+        f.is_unique.should be_false
+        f.maxlength.should eq(256)
+        f.minlength.should eq(8)
+        f.regex.should eq("^[a-zA-Z0-9@#$%^&+=*!~)(]{8,256}$")
+        f.regex_err_msg.should eq("Allowed chars: a-z A-Z 0-9 @ # $ % ^ & + = * ! ~ ) (")
+        f.other_attrs.should eq("")
+        f.css_classes.should eq("")
+        f.hint.should eq("")
+        f.warning.should eq("")
+        f.errors.should eq(Array(String).new)
+        f.group.should eq(1_u32)
+      end
+    end
+  end
+end
---
+++ src/fields/password.cr
@@ -0,0 +1,46 @@
+require "./field"
+
+module Fields
+  # Field for entering password.
+  struct PasswordField < Fields::Field
+    # Field type - Structure Name.
+    getter field_type : String = "PasswordField"
+    # Html tag: input type="url".
+    getter input_type : String = "password"
+    # Sets the value of an element.
+    property value : String | Nil
+    # Displays prompt text.
+    property placeholder : String
+    # The maximum number of characters allowed in the text.
+    property maxlength : UInt32
+    # The minimum number of characters allowed in the text.
+    property minlength : UInt32
+    # Regular expression to validate the value.
+    property regex : String
+    # Error message.
+    property regex_err_msg : String
+    # The unique value of a field in a collection.
+    property is_unique : Bool
+    # To optimize field traversal in the `paladins/check()` method.
+    # Hint: It is recommended not to change.
+    getter group : UInt32 = 1
+
+    def initialize(
+      @label : String = "",
+      @placeholder : String = "",
+      @maxlength : UInt32 = 256,
+      @minlength : UInt32 = 8,
+      @regex : String = "^[a-zA-Z0-9@#$%^&+=*!~)(]{8,256}$",
+      @regex_err_msg : String = "Allowed chars: a-z A-Z 0-9 @ # $ % ^ & + = * ! ~ ) (",
+      @is_hide : Bool = false,
+      @is_unique : Bool = false,
+      @is_required : Bool = false,
+      @is_disabled : Bool = false,
+      @is_readonly : Bool = false,
+      @other_attrs : String = "",
+      @css_classes : String = "",
+      @hint : String = "",
+      @warning : String = ""
+    ); end
+  end
+end
