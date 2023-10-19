1b046cf84156b1905cc5d917180c47bfd6c2ae6e
kebasyaty <kebasyaty@gmail.com>
2023-10-19 18:04:11 +0300

    Add I64Field

--- spec/fields/numbers_spec.cr
+++ spec/fields/numbers_spec.cr
@@ -19,8 +19,38 @@ describe Fields do
         f.is_required.should be_false
         f.is_unique.should be_false
         f.max.should eq(UInt32::MAX)
-        f.min.should eq(0)
-        f.step.should eq(1)
+        f.min.should eq(0_u32)
+        f.step.should eq(1_u32)
+        f.other_attrs.should eq("")
+        f.css_classes.should eq("")
+        f.hint.should eq("")
+        f.warning.should eq("")
+        f.errors.should eq(Array(String).new)
+        f.group.should eq(11_u32)
+      end
+    end
+  end
+
+  describe "Fields::I64Field" do
+    describe ".new" do
+      it "create an instance of the Int64 field" do
+        f = Fields::I64Field.new
+        f.id.should eq("")
+        f.label.should eq("")
+        f.field_type.should eq("I64Field")
+        f.input_type.should eq("number")
+        f.name.should eq("")
+        f.value.should be_nil
+        f.default.should be_nil
+        f.placeholder.should eq("")
+        f.is_disabled.should be_false
+        f.is_readonly.should be_false
+        f.is_hide.should be_false
+        f.is_required.should be_false
+        f.is_unique.should be_false
+        f.max.should eq(Int64::MAX)
+        f.min.should eq(0_i64)
+        f.step.should eq(1_i64)
         f.other_attrs.should eq("")
         f.css_classes.should eq("")
         f.hint.should eq("")
--- src/fields/numbers.cr
+++ src/fields/numbers.cr
@@ -27,11 +27,54 @@ module Fields
 
     def initialize(
       @label : String = "",
-      @default : String | Nil = nil,
+      @default : UInt32 | Nil = nil,
       @placeholder : String = "",
       @max : UInt32 = UInt32::MAX,
       @min : UInt32 = 0,
       @step : UInt32 = 1,
+      @is_hide : Bool = false,
+      @is_unique : Bool = false,
+      @is_required : Bool = false,
+      @is_disabled : Bool = false,
+      @is_readonly : Bool = false,
+      @other_attrs : String = "",
+      @css_classes : String = "",
+      @hint : String = "",
+      @warning : String = ""
+    ); end
+  end
+
+  # Field for entering integer 64-bit numbers.
+  struct I64Field < Fields::Field
+    # Field type - Structure Name.
+    getter field_type : String = "I64Field"
+    # Html tag: input type="url".
+    getter input_type : String = "number"
+    # Sets the value of an element.
+    property value : Int64 | Nil
+    # Value by default.
+    property default : Int64 | Nil
+    # Displays prompt text.
+    property placeholder : String
+    # The maximum number of characters allowed in the text.
+    property max : Int64
+    # The minimum number of characters allowed in the text.
+    property min : Int64
+    # Increment step for numeric fields.
+    property step : Int64
+    # The unique value of a field in a collection.
+    property is_unique : Bool
+    # To optimize field traversal in the `paladins/check()` method.
+    # Hint: It is recommended not to change.
+    getter group : UInt32 = 11
+
+    def initialize(
+      @label : String = "",
+      @default : Int64 | Nil = nil,
+      @placeholder : String = "",
+      @max : Int64 = Int64::MAX,
+      @min : Int64 = 0,
+      @step : Int64 = 1,
       @is_hide : Bool = false,
       @is_unique : Bool = false,
       @is_required : Bool = false,
