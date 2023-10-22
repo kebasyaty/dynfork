42b69c44d0da374d5d6189c85b43586d93d76af9
kebasyaty <kebasyaty@gmail.com>
2023-10-22 15:06:43 +0300

    Update test for Meta

--- spec/crymon/meta_spec.cr
+++ spec/crymon/meta_spec.cr
@@ -42,6 +42,16 @@ describe Crymon do
         m.default_value_map.should eq(Hash(String, String).new)
         m.ignore_fields.should eq(Hash(Array(String).new)
         m.choice_str_map.should eq(Hash(String, Array(String)).new)
+        m.choice_i64_map.should eq(Hash(String, Array(Int64)).new)
+        m.choice_f64_map.should eq(Hash(String, Array(Float64)).new)
+        m.model_json.should eq("")
+        m.db_query_docs_limit.should eq(1000_u32)
+        m.is_add_doc.should be_true
+        m.is_up_doc.should be_true
+        m.is_del_doc.should be_true
+        m.is_use_addition.should be_false
+        m.is_use_hooks.should be_false
+        m.is_use_hash_slug.should be_false
       end
     end
   end
