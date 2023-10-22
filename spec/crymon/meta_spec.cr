efad189d052e5d0e9a468768f2b2e95b4d1841ed
kebasyaty <kebasyaty@gmail.com>
2023-10-22 15:29:00 +0300

    Update Meta

--- spec/crymon/meta_spec.cr
+++ spec/crymon/meta_spec.cr
@@ -21,8 +21,8 @@ describe Crymon do
           choice_i64_map: Hash(String, Array(Int64)).new,
           choice_f64_map: Hash(String, Array(Float64)).new,
           model_json: "",
-          db_query_docs_limit: = 1000,
-          is_add_doc: = true,
+          db_query_docs_limit: 1000,
+          is_add_doc: true,
           is_up_doc: true,
           is_del_doc: true,
           is_use_addition: false,
@@ -40,7 +40,7 @@ describe Crymon do
         m.field_value_type_map.should eq(Hash(String, String).new)
         m.field_type_map.should eq(Hash(String, String).new)
         m.default_value_map.should eq(Hash(String, String).new)
-        m.ignore_fields.should eq(Hash(Array(String).new)
+        m.ignore_fields.should eq(Hash(Array(String)).new)
         m.choice_str_map.should eq(Hash(String, Array(String)).new)
         m.choice_i64_map.should eq(Hash(String, Array(Int64)).new)
         m.choice_f64_map.should eq(Hash(String, Array(Float64)).new)
--- src/crymon/model/meta.cr
+++ src/crymon/model/meta.cr
@@ -1,36 +1,36 @@
 module Crymon
   # Metadata for Model.
   struct Meta
-    getter model_name : String
-    getter app_name : String
-    getter unique_app_key : String
-    getter service_name : String
-    getter database_name : String
-    getter collection_name : String
-    getter db_query_docs_limit : UInt32
-    getter fields_count : UInt32
-    getter fields_name : Array(String)
-    getter is_add_doc : Bool
-    getter is_up_doc : Bool
-    getter is_del_doc : Bool
-    getter is_use_addition : Bool
-    getter is_use_hooks : Bool
-    getter is_use_hash_slug : Bool
+    getter model_name : String = ""
+    getter app_name : String = ""
+    getter unique_app_key : String = ""
+    getter service_name : String = ""
+    getter database_name : String = ""
+    getter collection_name : String = ""
+    getter db_query_docs_limit : UInt32 = 1000
+    getter fields_count : UInt32 = 0
+    getter fields_name : Array(String) = Array(String).new
+    getter is_add_doc : Bool = true
+    getter is_up_doc : Bool = true
+    getter is_del_doc : Bool = true
+    getter is_use_addition : Bool = false
+    getter is_use_hooks : Bool = false
+    getter is_use_hash_slug : Bool = false
     # Format: <field_name, field_value_type>
-    getter field_value_type_map : Hash(String, String)
+    getter field_value_type_map : Hash(String, String) = Hash(String, String).new
     # Format: <field_name, fields_type>
-    getter field_type_map : Hash(String, String)
+    getter field_type_map : Hash(String, String) = Hash(String, String).new
     # Format: <field_name, default_value>
-    getter default_value_map : Hash(String, String)
+    getter default_value_map : Hash(String, String) = Hash(String, String).new
     # List of field names that will not be saved to the database.
-    getter ignore_fields : Array(String)
+    getter ignore_fields : Array(String) = Array(String).new
     # Choice maps for fields type `choice`.
     # Format: HashMap<field_name, choices>
-    getter choice_str_map : Hash(String, Array(String))
-    getter choice_i64_map : Hash(String, Array(Int64))
-    getter choice_f64_map : Hash(String, Array(Float64))
+    getter choice_str_map : Hash(String, Array(String)) = Hash(String, Array(String)).new
+    getter choice_i64_map : Hash(String, Array(Int64)) = Hash(String, Array(Int64)).new
+    getter choice_f64_map : Hash(String, Array(Float64)) = Hash(String, Array(Float64)).new
     #
-    getter model_json : String
+    getter model_json : String = ""
   end
 
   def initialize(
