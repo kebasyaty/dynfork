b562597db4347060e2a7d16f4dd48293d1f42a11
kebasyaty <kebasyaty@gmail.com>
2023-10-22 14:47:56 +0300

    Update struct Meta

--- src/crymon/model/meta.cr
+++ src/crymon/model/meta.cr
@@ -1,4 +1,5 @@
 module Crymon
+  # Metadata for Model.
   struct Meta
     getter model_name : String
     getter app_name : String
@@ -6,26 +7,55 @@ module Crymon
     getter service_name : String
     getter database_name : String
     getter collection_name : String
-    getter db_query_docs_limit : Uint32
-    getter fields_count : Uint32
+    getter db_query_docs_limit : UInt32
+    getter fields_count : UInt32
     getter fields_name : Array(String)
     getter is_add_doc : Bool
     getter is_up_doc : Bool
     getter is_del_doc : Bool
+    getter is_use_addition : Bool
+    getter is_use_hooks : Bool
+    getter is_use_hash_slug : Bool
+    # Format: <field_name, field_value_type>
+    getter field_value_type_map : Hash(String, String)
+    # Format: <field_name, fields_type>
+    getter field_type_map : Hash(String, String)
+    # Format: <field_name, default_value>
+    getter default_value_map : Hash(String, String)
+    # List of field names that will not be saved to the database.
+    getter ignore_fields : Array(String)
+    # Choice maps for fields type `choice`.
+    # Format: HashMap<field_name, choices>
+    getter choice_str_map : Hash(String, Array(String))
+    getter choice_i64_map : Hash(String, Array(Int64))
+    getter choice_f64_map : Hash(String, Array(Float64))
+    #
+    getter model_json : String
   end
 
   def initialize(
-    @model_name : String = "",
-    @app_name : String = "",
-    @unique_app_key : String = "",
-    @service_name : String = "",
-    @database_name : String = "",
-    @collection_name : String = "",
+    @model_name : String,
+    @app_name : String,
+    @unique_app_key : String,
+    @service_name : String,
+    @database_name : String,
+    @collection_name : String,
+    @fields_count : UInt32,
+    @fields_name : Array(String),
+    @field_value_type_map : Hash(String, String),
+    @field_type_map : Hash(String, String),
+    @default_value_map : Hash(String, String),
+    @ignore_fields : Array(String),
+    @choice_str_map : Hash(String, Array(String)),
+    @choice_i64_map : Hash(String, Array(Int64)),
+    @choice_f64_map : Hash(String, Array(Float64)),
+    @model_json : String,
     @db_query_docs_limit : UInt32 = 1000,
-    @fields_count : UInt32 = 0,
-    @fields_name : Array(String) = Array(String).new,
     @is_add_doc : Bool = true,
     @is_up_doc : Bool = true,
-    @is_del_doc : Bool = true
+    @is_del_doc : Bool = true,
+    @is_use_addition : Bool = false,
+    @is_use_hooks : Bool = false,
+    @is_use_hash_slug : Bool = false
   ); end
 end
