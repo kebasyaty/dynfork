feb22539b14566914cfcac0f44c021f496e82fd4
kebasyaty <kebasyaty@gmail.com>
2023-10-22 14:56:40 +0300

    Update test for Meta

--- spec/crymon/meta_spec.cr
+++ spec/crymon/meta_spec.cr
@@ -4,7 +4,31 @@ describe Crymon do
   describe "Crymon::Meta" do
     describe ".new" do
       it "create instance of Meta" do
-        m = Crymon::Meta.new
+        m = Crymon::Meta.new(
+          model_name: "",
+          app_name: "",
+          unique_app_key: "",
+          service_name: "",
+          database_name: "",
+          collection_name: "",
+          fields_count: 0_u32,
+          fields_name: Array(String).new,
+          field_value_type_map: Hash(String, String).new,
+          field_type_map: Hash(String, String).new,
+          default_value_map: Hash(String, String).new,
+          ignore_fields: Array(String).new,
+          choice_str_map: Hash(String, Array(String)).new,
+          choice_i64_map: Hash(String, Array(Int64)).new,
+          choice_f64_map: Hash(String, Array(Float64)).new,
+          model_json: "",
+          db_query_docs_limit: = 1000,
+          is_add_doc: = true,
+          is_up_doc: true,
+          is_del_doc: true,
+          is_use_addition: false,
+          is_use_hooks: false,
+          is_use_hash_slug: false
+        )
         m.model_name.should eq("")
       end
     end
