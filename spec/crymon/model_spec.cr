392f96771b74a98b54a935b8b6fd247a477dc029
kebasyaty <kebasyaty@gmail.com>
2023-10-23 15:59:26 +0300

    Rename test Model

--- spec/crymon/model_spec.cr
+++ spec/crymon/model_spec.cr
@@ -3,9 +3,9 @@ require "../spec_helper"
 describe Crymon do
   describe "Crymon::Model" do
     describe ".new" do
-      it "create instance of Model" do
-        m = Helper::ModelName.new
-        m.model_name.should eq("ModelName")
+      it "create instance of empty Model" do
+        m = Helper::EmptyModel.new
+        m.model_name.should eq("EmptyModel")
         m.vars_count.should eq(0_i32)
         m.instance_vars_names.should eq(Array(String).new)
         m.instance_vars_types.should eq(Array(String).new)
