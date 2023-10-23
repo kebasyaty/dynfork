622fb092f8b027046431aa30bc0d461d6b6af208
kebasyaty <kebasyaty@gmail.com>
2023-10-23 15:53:12 +0300

    Fix test for Model

--- spec/crymon/model_spec.cr
+++ spec/crymon/model_spec.cr
@@ -6,7 +6,7 @@ describe Crymon do
       it "create instance of Model" do
         m = Helper::ModelName.new
         m.model_name.should eq("ModelName")
-        m.fields_count.should eq(0_i32)
+        m.vars_count.should eq(0_i32)
         m.instance_vars_names.should eq(Array(String).new)
         m.instance_vars_types.should eq(Array(String).new)
         m.instance_vars_name_and_type.should eq(Hash(String, String).new)
