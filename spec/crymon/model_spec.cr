201b5450d41428fa80502989e84fc27be126d11f
kebasyaty <kebasyaty@gmail.com>
2023-10-23 15:36:47 +0300

    Add macros

--- spec/crymon/model_spec.cr
+++ spec/crymon/model_spec.cr
@@ -5,7 +5,12 @@ describe Crymon do
     describe ".new" do
       it "create instance of Model" do
         m = Helper::ModelName.new
-        m.name.should eq("")
+        m.model_name.should eq("ModelName")
+        m.fields_count.should eq(0_i32)
+        m.instance_vars_names.should eq(Array(String).new)
+        m.instance_vars_types.should eq(Array(String).new)
+        m.instance_vars_name_and_type.should eq(Hash(String, String).new)
+        m.has_instance_var?("???").should be_false
       end
     end
   end
--- src/crymon/model/model.cr
+++ src/crymon/model/model.cr
@@ -1,5 +1,54 @@
 module Crymon
+  # ???
   abstract struct Model
-    property name : String = ""
+    # ???
+    def model_name : String
+      {{ @type.stringify }}.split("::")[-1]
+    end
+
+    # ???
+    def fields_count : Int32
+      {{ @type.instance_vars.size }}
+    end
+
+    # ???
+    def instance_vars_names : Array(String)
+      {% if @type.instance_vars.size > 0 %}
+        {{ @type.instance_vars.map &.name.stringify }}
+      {% else %}
+        Array(String).new
+      {% end %}
+    end
+
+    # ???
+    def instance_vars_types : Array(String)
+      {% if @type.instance_vars.size > 0 %}
+        {{ @type.instance_vars.map &.type.stringify }}
+      {% else %}
+        Array(String).new
+      {% end %}
+    end
+
+    # ???
+    # Format: <field_name, field_type>
+    def instance_vars_name_and_type : Hash(String, String)
+      {% if @type.instance_vars.size > 0 %}
+        Hash.zip(
+          {{ @type.instance_vars.map &.name.stringify }},
+          {{ @type.instance_vars.map &.type.stringify }}
+        )
+      {% else %}
+        Hash(String, String).new
+      {% end %}
+    end
+
+    # ???
+    def has_instance_var?(name) : Bool
+      {% if @type.instance_vars.size > 0 %}
+        {{ @type.instance_vars.map &.name.stringify }}.includes? name
+      {% else %}
+        false
+      {% end %}
+    end
   end
 end
