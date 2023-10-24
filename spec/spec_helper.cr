da8d3533633228c58a41380a63f3a8f675804392
kebasyaty <kebasyaty@gmail.com>
2023-10-24 09:07:57 +0300

    Add in test struct FilledModel

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -4,4 +4,15 @@ require "../src/crymon"
 module Helper
   # Model without variables and methods.
   struct EmptyModel < Crymon::Model; end
+
+  # Model with variables and methods.
+  struct FilledModel < Crymon::Model
+    getter name : String
+    getter age : Int32
+
+    def initialize(
+      @name : String,
+      @age : Int32
+    ); end
+  end
 end
