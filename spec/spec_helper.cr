7665f8cf8732a01d52968e4532188846fe8bffb9
kebasyaty <kebasyaty@gmail.com>
2023-10-26 08:29:34 +0300

    Update tests for Model

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,26 +1,28 @@
 require "spec"
 require "../src/crymon"
 
-APP_NAME        = "AppName"
-UNIQUE_APP_KEY  = "123"
-SERVICE_NAME    = "ServiceName"
-DATABASE_NAME   = "DatabaseName"
-COLLECTION_NAME = "CollectionName"
+module Settings
+  APP_NAME        = "AppName"
+  UNIQUE_APP_KEY  = "123"
+  SERVICE_NAME    = "ServiceName"
+  DATABASE_NAME   = "DatabaseName"
+  COLLECTION_NAME = "CollectionName"
+end
 
 module Helper
   # Model without variables and methods.
   @[Crymon::Metadata(
-    app_name: APP_NAME, unique_app_key: UNIQUE_APP_KEY,
-    service_name: SERVICE_NAME, database_name: DATABASE_NAME,
-    collection_name: COLLECTION_NAME
+    app_name: Settings::APP_NAME, unique_app_key: Settings::UNIQUE_APP_KEY,
+    service_name: Settings::SERVICE_NAME, database_name: Settings::DATABASE_NAME,
+    collection_name: Settings::COLLECTION_NAME
   )]
   struct EmptyModel < Crymon::Model; end
 
   # Model with variables and methods.
   @[Crymon::Metadata(
-    app_name: APP_NAME, unique_app_key: UNIQUE_APP_KEY,
-    service_name: SERVICE_NAME, database_name: DATABASE_NAME,
-    collection_name: COLLECTION_NAME, db_query_docs_limit: 2000_u32,
+    app_name: Settings::APP_NAME, unique_app_key: Settings::UNIQUE_APP_KEY,
+    service_name: Settings::SERVICE_NAME, database_name: Settings::DATABASE_NAME,
+    collection_name: Settings::COLLECTION_NAME, db_query_docs_limit: 2000_u32,
     ignore_fields: ["age", "birthday"]
   )]
   struct FilledModel < Crymon::Model
