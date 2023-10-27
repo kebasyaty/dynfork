c8652d468a60ac7669d397b828e0e804de65ef26
kebasyaty <kebasyaty@gmail.com>
2023-10-27 13:54:27 +0300

    Add comments to Model . meta

--- src/crymon/errors.cr
+++ src/crymon/errors.cr
@@ -10,5 +10,12 @@ module Crymon
         super(%(The "#{input_type}" invalid input type.))
       end
     end
+
+    # Error: Invalid input type.
+    class InvalidInputType < CrymonException
+      def initialize(input_type : String)
+        super(%(The "#{input_type}" invalid input type.))
+      end
+    end
   end
 end
