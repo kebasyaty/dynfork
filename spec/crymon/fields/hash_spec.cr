8018a098a8396ecbf18d908d56ec48828d60df4b
kebasyaty <kebasyaty@gmail.com>
2023-11-02 08:33:04 +0200

    Fix test for HashField

--- spec/crymon/fields/hash_spec.cr
+++ spec/crymon/fields/hash_spec.cr
@@ -5,23 +5,23 @@ describe Crymon::Fields::HashField do
     it "=> create instance of Hash field" do
       f = Crymon::Fields::HashField.new
       f.id.should eq("")
-      f.label.should eq("")
+      f.label.should eq("Hash ID")
       f.field_type.should eq("HashField")
       f.input_type.should eq("text")
       f.name.should eq("")
       f.value.should be_nil
       f.default.should be_nil
-      f.placeholder.should eq("")
+      f.placeholder.should eq("Enter the Document ID for MongoDB")
       f.is_disabled.should be_false
       f.is_readonly.should be_false
       f.is_hide.should be_true
       f.is_required.should be_false
-      f.is_unique.should be_false
+      f.is_unique.should be_true
       f.maxlength.should eq(12)
       f.minlength.should eq(12)
       f.other_attrs.should eq("")
       f.css_classes.should eq("")
-      f.hint.should eq("")
+      f.hint.should eq("For enter a document ID")
       f.warning.should eq("")
       f.errors.should eq(Array(String).new)
       f.group.should eq(1_u8)
