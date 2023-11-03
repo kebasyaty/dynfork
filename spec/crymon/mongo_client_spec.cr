18b199aa89d55fb65cb4f7982c1d802913890080
kebasyaty <kebasyaty@gmail.com>
2023-11-03 12:19:29 +0200

    Update test for mongodb client

--- spec/crymon/mongo_client_spec.cr
+++ spec/crymon/mongo_client_spec.cr
@@ -4,8 +4,6 @@ describe "Cryomongo" do
   it "=> initialize mongodb client" do
     # Create a Mongo client, using a standard mongodb connection string.
     client : Mongo::Client = Mongo::Client.new # defaults to: "mongodb://localhost:27017"
-    # The overwhelming majority of programs should use a single client and should not bother with closing clients.
-    # Otherwise, to free the underlying resources a client must be manually closed.
 
     # Get database and collection.
     # database = client["database_name"]
@@ -18,7 +16,9 @@ describe "Cryomongo" do
     # bson.not_nil!.["two"].should eq(2)
     # collection.delete_one({two: 2})
     # collection.count_documents.should eq(0)
-    #
-    # client.close
+
+    # The overwhelming majority of programs should use a single client and should not bother with closing clients.
+    # Otherwise, to free the underlying resources a client must be manually closed.
+    client.close
   end
 end
