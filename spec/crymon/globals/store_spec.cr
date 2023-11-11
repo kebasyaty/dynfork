0303b21ecc47417a793b782777459c56894ade68
kebasyaty <kebasyaty@gmail.com>
2023-11-11 17:26:49 +0200

    Update tests

--- spec/crymon/globals/store_spec.cr
+++ spec/crymon/globals/store_spec.cr
@@ -2,11 +2,18 @@ require "../../spec_helper"
 
 describe Crymon::Globals do
   describe "store" do
-    it "=> store_metadata", tags: "globals_store" do
-      # ...
+    it "=> checking crymon global settings without database name" do
+      Crymon::Globals.store_app_name = "AppName"
+      Crymon::Globals.store_unique_app_key = "RT0839370A074kVh"
+      #
+      Crymon::Globals::ValidationStoreSettings.validation
+      #
+      Crymon::Globals.store_app_name.should eq("AppName")
+      Crymon::Globals.store_unique_app_key.should eq("RT0839370A074kVh")
+      Crymon::Globals.store_database_name.should eq("AppName_RT0839370A074kVh")
     end
 
-    it "=> validation store_settings", tags: "globals_store" do
+    it "=> checking crymon global settings with database name", tags: "globals_store" do
       Crymon::Globals.store_app_name = "AppName"
       Crymon::Globals.store_unique_app_key = "RT0839370A074kVh"
       Crymon::Globals.store_database_name = "DatabaseName360"
