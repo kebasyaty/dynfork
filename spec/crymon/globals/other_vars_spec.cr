96ae0ef424f87aab53431a5d6b8a6a5b2e160abd
kebasyaty <kebasyaty@gmail.com>
2023-11-12 16:59:28 +0200

    Гзвфеу other_vars_spec

--- spec/crymon/globals/other_vars_spec.cr
+++ spec/crymon/globals/other_vars_spec.cr
@@ -1,9 +1,17 @@
 require "../../spec_helper"
 
 describe Crymon::Globals do
-  it "=> cache_metadata - type checking", tags: "global_other_vars" do
+  it "=> cache_metadata - check original value", tags: "global_other_vars" do
     Crymon::Globals.cache_metadata.should eq(
       Hash(String, Crymon::Globals::CacheMetaDataType).new
     )
+  end
+
+  it "=> cache_mongo_client - check original value", tags: "global_other_vars" do
+    Crymon::Globals.cache_mongo_client.should eq(Mongo::Client.new)
+  end
+
+  it "=> cache_super_collection_name - check original value", tags: "global_other_vars" do
+    Crymon::Globals.cache_super_collection_name.should eq("SUPER_COLLECTION")
   end
 end
