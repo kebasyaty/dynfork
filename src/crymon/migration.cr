bcf851bfdce83ac8cc7a810133fb56010e19211a
kebasyaty <kebasyaty@gmail.com>
2023-11-13 16:37:45 +0200

    Update Migration

--- src/crymon/migration.cr
+++ src/crymon/migration.cr
@@ -3,6 +3,15 @@
 # your models (adding a field, deleting a collection, etc.) into
 # your database schema.
 module Crymon::Migration
+  # To control the status of Models in the super collection.
+  struct ModelState
+    getter collection_name : String
+    getter field_list : Array(String)
+    getter field_types : Hash(String, Crymon::Globals::FieldTypes)
+    property is_updated_state : Bool = false
+    getter is_model : Bool = true
+  end
+
   # Monitoring and update the database state for the application.
   struct Monitor
     def initialize(
@@ -25,11 +34,18 @@ module Crymon::Migration
     # <br>
     # 2) Update the state of Models in the super collection.
     def refresh
-      # Get super collection.
-      super_collection = Crymon::Globals.cache_mongo_client[
-        Crymon::Globals.cache_database_name][Crymon::Globals.cache_super_collection_name]
-      # Fetch a Cursor pointing to the super collection.
-      cursor = super_collection.find
+      # Get database and super collection.
+      database = Crymon::Globals.cache_mongo_client[Crymon::Globals.cache_database_name]
+      super_collection = database[Crymon::Globals.cache_super_collection_name]
+      # Reset Models state information.
+      filter = {is_model: true}
+      if super_collection.count_documents(filter) > 0
+        # Fetch a Cursor pointing to the super collection.
+        cursor = super_collection.find(filter)
+        cursor.each { |document|
+        # ...
+        }
+      end
     end
   end
 end
