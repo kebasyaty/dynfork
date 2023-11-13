324d950011894b1f48b7ba2da426786722ce69a6
kebasyaty <kebasyaty@gmail.com>
2023-11-13 13:07:41 +0200

    Update tests for Migration

--- spec/crymon/migration_spec.cr
+++ spec/crymon/migration_spec.cr
@@ -3,29 +3,28 @@ require "../spec_helper"
 describe Crymon::Migration::Monitor do
   describe ".new" do
     it "=> create instance without database name", tags: "migration" do
-      m = Crymon::Migration::Monitor.new(
+      Crymon::Migration::Monitor.new(
         "app_name": "AppName",
         "unique_app_key": "0w7n5731X13s1641",
         "mongo_uri": "mongodb://localhost:27017"
       )
       #
-      m.app_name.should eq("AppName")
-      m.unique_app_key.should eq("0w7n5731X13s1641")
-      m.database_name.should eq("")
-      m.mongo_uri.should eq("mongodb://localhost:27017")
+      Crymon::Globals.cache_app_name.should eq("AppName")
+      Crymon::Globals.cache_unique_app_key.should eq("0w7n5731X13s1641")
+      Crymon::Globals.cache_database_name.should eq("AppName_0w7n5731X13s1641")
     end
 
     it "=> create instance with database name", tags: "migration" do
-      m = Crymon::Migration::Monitor.new(
+      Crymon::Migration::Monitor.new(
         "app_name": "AppName",
-        "unique_app_key": "0w7n5731X13s1641",
+        "unique_app_key": "0585I0S5huR5r08q",
         "database_name": "DatabaseName360",
         "mongo_uri": "mongodb://localhost:27017"
       )
-      m.app_name.should eq("AppName")
-      m.unique_app_key.should eq("0w7n5731X13s1641")
-      m.database_name.should eq("DatabaseName360")
-      m.mongo_uri.should eq("mongodb://localhost:27017")
+      #
+      Crymon::Globals.cache_app_name.should eq("AppName")
+      Crymon::Globals.cache_unique_app_key.should eq("0585I0S5huR5r08q")
+      Crymon::Globals.cache_database_name.should eq("DatabaseName360")
     end
   end
 end
--- src/crymon/migration.cr
+++ src/crymon/migration.cr
@@ -5,26 +5,18 @@
 module Crymon::Migration
   # Monitoring and updating the database of application.
   struct Monitor
-    getter app_name : String
-    getter unique_app_key : String
-    getter database_name : String
-    getter mongo_uri : String
-
     def initialize(
-      @app_name : String,
-      @unique_app_key : String,
-      @mongo_uri : String,
-      @database_name : String = ""
+      app_name : String,
+      unique_app_key : String,
+      mongo_uri : String,
+      database_name : String = ""
     )
-    end
-
-    # Update global storage state.
-    def set_global_data
-      Crymon::Globals.cache_app_name = @app_name
-      Crymon::Globals.cache_unique_app_key = @unique_app_key
-      Crymon::Globals.cache_mongo_client = Mongo::Client.new(@mongo_uri)
+      # Update global storage state.
+      Crymon::Globals.cache_app_name = app_name
+      Crymon::Globals.cache_unique_app_key = unique_app_key
+      Crymon::Globals.cache_mongo_client = Mongo::Client.new(mongo_uri)
       unless @database_name.empty?
-        Crymon::Globals.cache_database_name = @database_name
+        Crymon::Globals.cache_database_name = database_name
       end
       Crymon::Globals::ValidationCacheSettings.validation
     end
