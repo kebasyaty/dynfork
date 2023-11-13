c9ac5c287ef1ca9e14da43f6d72503656113cfed
kebasyaty <kebasyaty@gmail.com>
2023-11-13 12:13:19 +0200

    Update Migration

--- src/crymon/migration.cr
+++ src/crymon/migration.cr
@@ -3,5 +3,29 @@
 # your models (adding a field, deleting a collection, etc.) into
 # your database schema.
 module Crymon::Migration
-  # ...
+  # For monitoring the state of models.
+  struct Monitor
+    getter app_name : String
+    getter unique_app_key : String
+    getter database_name : String
+    getter mongo_uri : String
+
+    def initialize(
+      @app_name : String,
+      @unique_app_key : String,
+      @mongo_uri : String,
+      @database_name : String = ""
+    )
+    end
+
+    # Update global storage state.
+    def set_global_data
+      Crymon::Globals.cache_app_name = @app_name
+      Crymon::Globals.cache_unique_app_key = @unique_app_key
+      Crymon::Globals.cache_mongo_client = Mongo::Client.new(@mongo_uri)
+      unless @database_name.empty?
+        Crymon::Globals.cache_database_name = @database_name
+      end
+    end
+  end
 end
