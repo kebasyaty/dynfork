6303966bcbc9b389c3e88ee8f85bc97502e1bfc6
kebasyaty <kebasyaty@gmail.com>
2023-11-21 14:16:59 +0200

    Fix doc comment

--- src/crymon/model/meta.cr
+++ src/crymon/model/meta.cr
@@ -5,7 +5,7 @@ module Crymon
   # <br>
   # | Parameter:          | Default:     | Description:                                                                                         |
   # | :------------------ | :----------- | :--------------------------------------------------------------------------------------------------- |
-  # | service_name        | no           | **Examples:** Accounts | Smartphones | Washing machines | etc ...                                    |                                        |
+  # | service_name        | no           | **Examples:** Accounts | Smartphones | Washing machines | etc ...                                    |
   # | db_query_docs_limit | 1000         | limiting query results.                                                                              |
   # | is_add_doc          | true         | Create documents in the database. **false** - Alternatively, use it to validate data from web forms. |
   # | is_up_doc           | true         | Update documents in the database.                                                                    |
