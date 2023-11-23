e5eaaee03778a3113a30e269548d53eac7b6e956
kebasyaty <kebasyaty@gmail.com>
2023-11-23 15:50:48 +0200

    fix docs

--- src/crymon/implement/hooks_impl.cr
+++ src/crymon/implement/hooks_impl.cr
@@ -2,21 +2,21 @@ module Crymon::Implement
   # Methods that are called at different stages when accessing the database.
   abstract struct Hooks
     # Called before a new document is created in the database.
-    private def pre_create; end
+    def pre_create; end
 
     # Called after a new document has been created in the database.
-    private def post_create; end
+    def post_create; end
 
     # Called before updating an existing document in the database.
-    private def pre_update; end
+    def pre_update; end
 
     # Called after an existing document in the database is updated.
-    private def post_update; end
+    def post_update; end
 
     # Called before deleting an existing document in the database.
-    private def pre_delete; end
+    def pre_delete; end
 
     # Called after an existing document in the database has been deleted.
-    private def post_delete; end
+    def post_delete; end
   end
 end
