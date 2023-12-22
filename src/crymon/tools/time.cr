6d98c650779a7a23d027f7b02f4016a671bfc544
kebasyaty <kebasyaty@gmail.com>
2023-12-22 21:21:11 +0200

    Update tool Time

--- src/crymon/tools/time.cr
+++ src/crymon/tools/time.cr
@@ -5,10 +5,11 @@ module Crymon::Tools::Time
   # _**Formats:** dd-mm-yyyy | dd/mm/yyyy | dd.mm.yyyy |
   # yyyy-mm-dd | yyyy/mm/dd | yyyy.mm.dd_
   def date_parse(date : String) : Time
-    if md : Regex::MatchData? = date.match(/^([0-9]{4})[-\/\.]([0-9]{2})[-\/\.]([0-9]{2})$/)
+    md : Regex::MatchData?
+    if md = date.match(/^([0-9]{4})[-\/\.]([0-9]{2})[-\/\.]([0-9]{2})$/)
       date = "#{md[1]}-#{md[2]}-#{md[3]}"
-    elsif md2 : Regex::MatchData? = date.match(/^([0-9]{2})[-\/\.]([0-9]{2})[-\/\.]([0-9]{4})$/)
-      date = "#{md2[3]}-#{md2[2]}-#{md2[1]}"
+    elsif md = date.match(/^([0-9]{2})[-\/\.]([0-9]{2})[-\/\.]([0-9]{4})$/)
+      date = "#{md[3]}-#{md[2]}-#{md[1]}"
     else
       raise "Invalid date."
     end
@@ -20,10 +21,11 @@ module Crymon::Tools::Time
   # _**Formats:** dd-mm-yyyy hh:mm | dd/mm/yyyy hh:mm | dd.mm.yyyy hh:mm |
   # yyyy-mm-dd hh:mm | yyyy/mm/dd hh:mm | yyyy.mm.dd hh:mm_
   def datetime_parse(datetime : String) : Time
-    if md : Regex::MatchData? = datetime.match(/^([0-9]{4})[-\/\.]([0-9]{2})[-\/\.]([0-9]{2})/)
-      datetime = "#{md[1]}-#{md[2]}-#{md[3]}#{datetime[10..]}"
-    elsif md2 : Regex::MatchData? = datetime.match(/^([0-9]{2})[-\/\.]([0-9]{2})[-\/\.]([0-9]{4})/)
-      datetime = "#{md2[3]}-#{md2[2]}-#{md2[1]}#{datetime[10..]}"
+    md : Regex::MatchData?
+    if md = datetime.match(/^([0-9]{4})[-\/\.]([0-9]{2})[-\/\.]([0-9]{2}) ([0-9]{2}:[0-9]{2})$/)
+      datetime = "#{md[1]}-#{md[2]}-#{md[3]} #{md[4]}"
+    elsif md = datetime.match(/^([0-9]{2})[-\/\.]([0-9]{2})[-\/\.]([0-9]{4}) ([0-9]{2}:[0-9]{2})$/)
+      datetime = "#{md[3]}-#{md[2]}-#{md[1]} #{md[4]}"
     else
       raise "Invalid date."
     end
