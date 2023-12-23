24d596bf7a4061cbdfefd39b5d0a0243395807fc
kebasyaty <kebasyaty@gmail.com>
2023-12-23 09:10:05 +0200

    Update tool Time

--- src/crymon/tools/time.cr
+++ src/crymon/tools/time.cr
@@ -5,30 +5,22 @@ module Crymon::Tools::Time
   # _**Formats:** dd-mm-yyyy | dd/mm/yyyy | dd.mm.yyyy |
   # yyyy-mm-dd | yyyy/mm/dd | yyyy.mm.dd_
   def date_parse(date : String) : Time
-    md : Regex::MatchData?
-    if md = date.match(/^([0-9]{4})[-\/\.]([0-9]{2})[-\/\.]([0-9]{2})$/)
-      date = "#{md[1]}-#{md[2]}-#{md[3]}"
-    elsif md = date.match(/^([0-9]{2})[-\/\.]([0-9]{2})[-\/\.]([0-9]{4})$/)
-      date = "#{md[3]}-#{md[2]}-#{md[1]}"
-    else
-      raise "Invalid date."
-    end
-    Time.parse_utc(date, "%F")
+    md = date.match(/^(?P<y>[0-9]{4})[-\/\.](?P<m>[0-9]{2})[-\/\.](?P<d>[0-9]{2})$/) ||
+         date.match(/^(?P<d>[0-9]{2})[-\/\.](?P<m>[0-9]{2})[-\/\.](?P<y>[0-9]{4})$/)
+    raise "Invalid date." if md.nil?
+    Time.parse_utc("#{md["y"]}-#{md["m"]}-#{md["d"]}", "%F")
   end
 
   # Converting string DateTime to object Time.
   # <br>
   # _**Formats:** dd-mm-yyyy hh:mm | dd/mm/yyyy hh:mm | dd.mm.yyyy hh:mm |
-  # yyyy-mm-dd hh:mm | yyyy/mm/dd hh:mm | yyyy.mm.dd hh:mm_
+  # dd-mm-yyyyThh:mm | dd/mm/yyyyThh:mm | dd.mm.yyyyThh:mm |
+  # yyyy-mm-dd hh:mm | yyyy/mm/dd hh:mm | yyyy.mm.dd hh:mm |
+  # yyyy-mm-ddThh:mm | yyyy/mm/ddThh:mm | yyyy.mm.ddThh:mm_
   def datetime_parse(datetime : String) : Time
-    md : Regex::MatchData?
-    if md = datetime.match(/^([0-9]{4})[-\/\.]([0-9]{2})[-\/\.]([0-9]{2}) ([0-9]{2}:[0-9]{2})$/)
-      datetime = "#{md[1]}-#{md[2]}-#{md[3]} #{md[4]}"
-    elsif md = datetime.match(/^([0-9]{2})[-\/\.]([0-9]{2})[-\/\.]([0-9]{4}) ([0-9]{2}:[0-9]{2})$/)
-      datetime = "#{md[3]}-#{md[2]}-#{md[1]} #{md[4]}"
-    else
-      raise "Invalid date."
-    end
-    Time.parse_utc(datetime, "%F %H:%M")
+    md = datetime.match(/^(?P<y>[0-9]{4})[-\/\.](?P<m>[0-9]{2})[-\/\.](?P<d>[0-9]{2})(?:T|\s)(?<t>[0-9]{2}:[0-9]{2})$/) ||
+         datetime.match(/^(?P<d>[0-9]{2})[-\/\.](?P<m>[0-9]{2})[-\/\.](?P<y>[0-9]{4})(?:T|\s)(?<t>[0-9]{2}:[0-9]{2})$/)
+    raise "Invalid date." if md.nil?
+    Time.parse_utc("#{md["y"]}-#{md["m"]}-#{md["d"]} #{md["t"]}", "%F %H:%M")
   end
 end
