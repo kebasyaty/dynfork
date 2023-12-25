f546814575f997cf913dc1d12b77c1ba199c7f12
kebasyaty <kebasyaty@gmail.com>
2023-12-25 12:20:01 +0200

    add OutputData struct

--- src/crymon/model/check.cr
+++ src/crymon/model/check.cr
@@ -1,12 +1,31 @@
 # Validation of Model data before saving to the database.
 module Crymon::Check
+  # Output data for the Save method.
+  struct OutputData
+    getter data : BSON
+    getter is_valid : Bool
+
+    def initialize(@data : BSON, @is_valid : Bool); end
+  end
+
+  # Check data validity.
+  # NOTE: The main use is to check data from web forms.
+  def is_valid : Bool
+    self.check(true).is_valid
+  end
+
   # Validation of Model data.
-  def check
+  private def check(only_validation : Bool = false) : OutputData
     # Get model key.
     model_key : String = self.model_key
     # Does the document exist in the database?
     is_updated : Bool = !@hash.value.empty?
     # Is there any incorrect data?
     is_error_symptom : Bool = false
+    # ???
+    data_bson : BSON = BSON.new
+
+    # --------------------------------------------------------------------------
+    OutputData.new(data_bson, is_error_symptom)
   end
 end
