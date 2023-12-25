2d37805993731ff879b6d2501d9853a18009d54b
kebasyaty <kebasyaty@gmail.com>
2023-12-25 11:44:03 +0200

    update check method

--- src/crymon/model/check.cr
+++ src/crymon/model/check.cr
@@ -2,6 +2,8 @@
 module Crymon::Check
   # Validation of Model data.
   def check
+    # Get model key.
+    model_key : String = self.model_key
     # Does the document exist in the database?
     is_updated : Bool = !@hash.value.empty?
     # Is there any incorrect data?
