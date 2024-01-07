07ba2473af27233714bb7c3f1ef90cb0f31b3a5e
kebasyaty <kebasyaty@gmail.com>
2024-01-07 10:42:32 +0200

    update

--- spec/dependencies/pluto_spec.cr
+++ spec/dependencies/pluto_spec.cr
@@ -11,4 +11,13 @@ describe "Crystal Pluto" do
     image_jpeg.height.should eq(100)
     image_jpeg.size.should eq(10000)
   end
+
+  it "=> image png", tags: "pluto" do
+    image_png = File.open("pictures/pluto.png") do |file|
+      Pluto::ImageRGBA.from_png(file)
+    end
+    image_png.width.should eq(100)
+    image_png.height.should eq(100)
+    image_png.size.should eq(0)
+  end
 end
