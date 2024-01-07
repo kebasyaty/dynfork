b5bbd55be77d04e3d3e2c0a9c0d54180a4f67fa2
kebasyaty <kebasyaty@gmail.com>
2024-01-07 07:44:16 +0200

    update test

--- spec/dependencies/validator_spec.cr
+++ spec/dependencies/validator_spec.cr
@@ -2,9 +2,12 @@ require "../spec_helper"
 
 # https://github.com/Nicolab/crystal-validator
 describe "Crystal Validator" do
-  it "=> testing lib", tags: "validator" do
+  it "=> testing email", tags: "validator" do
     Valid.email?("contact@example.org").should be_true
     Valid.email?("contact@@example.org").should be_false
+  end
+
+  it "=> testing url", tags: "validator" do
     Valid.url?("https://github.com/Nicolab/crystal-validator").should be_true
     Valid.url?("http://github.com/Nicolab/crystal-validator").should be_true
     Valid.url?("https:///github.com/Nicolab/crystal-validator").should be_false
