6319d3e642d6fcb6b697c1bcb11d642e2ca0ad42
kebasyaty <kebasyaty@gmail.com>
2024-01-08 10:39:04 +0200

    update

--- spec/dependencies/pluto_spec.cr
+++ spec/dependencies/pluto_spec.cr
@@ -2,7 +2,7 @@ require "../spec_helper"
 
 # https://github.com/phenopolis/pluto
 describe "Crystal Pluto" do
-  # sudo apt -y install libturbojpeg0-dev
+  # sudo apt -y install libturbojpeg0-dev | libjpeg-turbo8-dev
   it "=> image jpeg", tags: "pluto" do
     image_jpeg = File.open("pictures/pluto.jpg") do |file|
       Pluto::ImageRGBA.from_jpeg(file)
@@ -12,6 +12,10 @@ describe "Crystal Pluto" do
     image_jpeg.size.should eq(10000)
   end
 
+  # TEMP_DEB="$(mktemp)"
+  # wget --no-check-certificate -O "$TEMP_DEB" 'http://ftp.uk.debian.org/debian/pool/main/libs/libspng/libspng-dev_0.7.3-3_amd64.deb'
+  # sudo dpkg -i "$TEMP_DEB"
+  # rm -f "$TEMP_DEB"
   it "=> image png", tags: "pluto" do
     image_png = File.open("pictures/pluto.png") do |file|
       Pluto::ImageRGBA.from_png(file)
@@ -19,14 +23,5 @@ describe "Crystal Pluto" do
     image_png.width.should eq(100)
     image_png.height.should eq(100)
     image_png.size.should eq(0)
-  end
-
-  it "=> image webp", tags: "pluto" do
-    image_webp = File.open("pictures/pluto.webp") do |file|
-      Pluto::ImageRGBA.from_webp(file)
-    end
-    image_webp.width.should eq(100)
-    image_webp.height.should eq(100)
-    image_webp.size.should eq(0)
   end
 end
