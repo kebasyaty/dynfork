dda36eafc97bbd4a09d549ccdf59718713030834
kebasyaty <kebasyaty@gmail.com>
2024-01-08 11:32:42 +0200

    update

--- spec/dependencies/pluto_spec.cr
+++ spec/dependencies/pluto_spec.cr
@@ -4,7 +4,7 @@ require "../spec_helper"
 describe "Crystal Pluto" do
   # sudo apt -y install libturbojpeg0-dev
   it "=> image jpeg", tags: "pluto" do
-    image_jpeg = File.open("pictures/pluto.jpg") do |file|
+    image_jpeg = File.open("pictures/pluto_1.jpg") do |file|
       Pluto::ImageRGBA.from_jpeg(file)
     end
     image_jpeg.width.should eq(100)
@@ -19,7 +19,7 @@ describe "Crystal Pluto" do
   # sudo dpkg -i "$TEMP_DEB" &&
   # rm -f "$TEMP_DEB"
   it "=> image png", tags: "pluto" do
-    image_png = File.open("pictures/pluto.png") do |file|
+    image_png = File.open("pictures/pluto_2.png") do |file|
       Pluto::ImageRGBA.from_png(file)
     end
     image_png.width.should eq(100)
@@ -33,7 +33,7 @@ describe "Crystal Pluto" do
   # sudo wget --no-check-certificate 'http://ftp.uk.debian.org/debian/pool/main/libw/libwebp/libsharpyuv-dev_1.3.2-0.3_amd64.deb' &&
   # sudo dpkg -i 'libsharpyuv-dev_1.3.2-0.3_amd64.deb'
   it "=> image webp", tags: "pluto" do
-    image_webp = File.open("pictures/pluto.webp") do |file|
+    image_webp = File.open("pictures/pluto_3.webp") do |file|
       Pluto::ImageRGBA.from_webp(file)
     end
     image_webp.width.should eq(100)
