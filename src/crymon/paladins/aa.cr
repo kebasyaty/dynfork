51d33645cc897eb100f3ca264d413f3f79067aa8
kebasyaty <kebasyaty@gmail.com>
2024-01-09 12:03:54 +0200

    update AA

--- src/crymon/paladins/aa.cr
+++ src/crymon/paladins/aa.cr
@@ -43,31 +43,31 @@ module Crymon
     # Called before a new document is created in the database.
     # <br>
     # WARNING: The method is called automatically.
-    private def pre_create; end
+    def pre_create; end
 
     # Called after a new document has been created in the database.
     # <br>
     # WARNING: The method is called automatically.
-    private def post_create; end
+    def post_create; end
 
     # Called before updating an existing document in the database.
     # <br>
     # WARNING: The method is called automatically.
-    private def pre_update; end
+    def pre_update; end
 
     # Called after an existing document in the database is updated.
     # <br>
     # WARNING: The method is called automatically.
-    private def post_update; end
+    def post_update; end
 
     # Called before deleting an existing document in the database.
     # <br>
     # WARNING: The method is called automatically.
-    private def pre_delete; end
+    def pre_delete; end
 
     # Called after an existing document in the database has been deleted.
     # <br>
     # WARNING: The method is called automatically.
-    private def post_delete; end
+    def post_delete; end
   end
 end
