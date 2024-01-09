8db69bf4168984be769c7f9affb7608f05502faf
kebasyaty <kebasyaty@gmail.com>
2024-01-09 11:59:13 +0200

    update tests for validator

--- spec/dependencies/validator_spec.cr
+++ spec/dependencies/validator_spec.cr
@@ -17,15 +17,39 @@ describe "Crystal Validator" do
     Valid.url?("http://github.com/Nicolab/crystal-validator").should be_true
   end
 
+  it "=> testing ip", tags: "validator" do
+    # Negative:
+    # Valid.ip?("").should be_false # Index out of bounds (IndexError)
+    # Valid.ip?("::").should be_false # Index out of bounds (IndexError)
+    Valid.ip?("10.0.0.0/8").should be_false
+    Valid.ip?("172.16.0.0/12").should be_false
+    Valid.ip?("192.168.0.0/16").should be_false
+    Valid.ip?("169.254.0.0/24").should be_false
+    Valid.ip?("FF02:0:0:0:0:1:FF00::/104").should be_false
+    Valid.ip?("FF02:0:0:0:0:1:FF00::").should be_false
+    Valid.ip?("FF02:0:0:0:0:1:FF00").should be_false
+    Valid.ip?("2345:425:2CA1:0000:0000:567:5673:23b5/64").should be_false
+    # Positive:
+    Valid.ip?("192.0.2.2").should be_true
+    Valid.ip?("100.64.2.2").should be_true
+    Valid.ip?("172.16.0.0").should be_true
+    Valid.ip?("192.168.0.0").should be_true
+    Valid.ip?("169.254.1.0").should be_true
+    Valid.ip?("169.254.254.255").should be_true
+    Valid.ip?("169.254.0.0").should be_true
+    Valid.ip?("169.254.255.0").should be_true
+    Valid.ip?("255.255.255.255").should be_true
+    Valid.ip?("198.18.0.0").should be_true
+    Valid.ip?("::ffff:192.0.2.1").should be_true
+    Valid.ip?("3001:0da8:75a3:0000:0000:8a2e:0370:7334").should be_true
+    Valid.ip?("2345:0425:2CA1:0000:0000:0567:5673:23b5").should be_true
+    Valid.ip?("2345:0425:2CA1::0567:5673:23b5").should be_true
+  end
+
   it "=> testing color", tags: "validator" do
     # Negative:
     Valid.color?("").should be_false
     Valid.color?("#f2ewq").should be_false
-    Valid.color?(10).should be_false
-    Valid.color?(10.1).should be_false
-    Valid.color?(nil).should be_false
-    Valid.color?(Array(String)).should be_false
-    Valid.color?(Hash(String, String)).should be_false
     # Positive:
     Valid.color?("#fff").should be_true
     Valid.color?("#f2f2f2").should be_true
