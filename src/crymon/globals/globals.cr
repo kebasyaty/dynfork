7e1d53ae9a84c69a517ddd880b96a06ab2d52049
kebasyaty <kebasyaty@gmail.com>
2024-01-10 10:12:24 +0200

    update globals

--- src/crymon/globals/globals.cr
+++ src/crymon/globals/globals.cr
@@ -36,29 +36,26 @@ module Crymon::Globals
   # All field types.
   alias FieldTypes = Crymon::Fields::URLField | Crymon::Fields::TextField |
                      Crymon::Fields::SlugField | Crymon::Fields::PhoneField |
-                     Crymon::Fields::PasswordField | Crymon::Fields::U32Field |
-                     Crymon::Fields::I64Field | Crymon::Fields::F64Field |
-                     Crymon::Fields::IPField | Crymon::Fields::ImageField |
-                     Crymon::Fields::HashField | Crymon::Fields::FileField |
-                     Crymon::Fields::EmailField | Crymon::Fields::DateTimeField |
-                     Crymon::Fields::DateField | Crymon::Fields::ColorField |
-                     Crymon::Fields::ChoiceU32Field | Crymon::Fields::ChoiceU32MultField |
-                     Crymon::Fields::ChoiceU32DynField | Crymon::Fields::ChoiceU32MultDynField |
-                     Crymon::Fields::ChoiceTextField | Crymon::Fields::ChoiceTextMultField |
-                     Crymon::Fields::ChoiceTextDynField | Crymon::Fields::ChoiceTextMultDynField |
-                     Crymon::Fields::ChoiceI64Field | Crymon::Fields::ChoiceI64MultField |
-                     Crymon::Fields::ChoiceI64DynField | Crymon::Fields::ChoiceI64MultDynField |
-                     Crymon::Fields::ChoiceF64Field | Crymon::Fields::ChoiceF64MultField |
-                     Crymon::Fields::ChoiceF64DynField | Crymon::Fields::ChoiceF64MultDynField |
-                     Crymon::Fields::BoolField
+                     Crymon::Fields::PasswordField | Crymon::Fields::I64Field |
+                     Crymon::Fields::F64Field | Crymon::Fields::IPField |
+                     Crymon::Fields::ImageField | Crymon::Fields::HashField |
+                     Crymon::Fields::FileField | Crymon::Fields::EmailField |
+                     Crymon::Fields::DateTimeField | Crymon::Fields::DateField |
+                     Crymon::Fields::ColorField | Crymon::Fields::ChoiceTextField |
+                     Crymon::Fields::ChoiceTextMultField | Crymon::Fields::ChoiceTextDynField |
+                     Crymon::Fields::ChoiceTextMultDynField | Crymon::Fields::ChoiceI64Field |
+                     Crymon::Fields::ChoiceI64MultField | Crymon::Fields::ChoiceI64DynField |
+                     Crymon::Fields::ChoiceI64MultDynField | Crymon::Fields::ChoiceF64Field |
+                     Crymon::Fields::ChoiceF64MultField | Crymon::Fields::ChoiceF64DynField |
+                     Crymon::Fields::ChoiceF64MultDynField | Crymon::Fields::BoolField
 
   # Field value types.
-  alias ValueTypes = String | UInt32 | Int64 | Float64 | Crymon::Globals::ImageData |
-                     Crymon::Globals::FileData | Array(UInt32) | Array(String) | Array(Int64) |
+  alias ValueTypes = String | Int64 | Float64 | Crymon::Globals::ImageData |
+                     Crymon::Globals::FileData | Array(String) | Array(Int64) |
                      Array(Float64) | Bool | Nil
 
   # Data types to select in dynamic fields.
-  alias DataDynamicTypes = Array(String | UInt32 | Int64 | Float64)
+  alias DataDynamicTypes = Array(String | Int64 | Float64)
 
   # A type for caching Metadata of Model.
   alias CacheMetaDataType = NamedTuple(
