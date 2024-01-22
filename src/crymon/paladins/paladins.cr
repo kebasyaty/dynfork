a6a5f8e40a9c671d11f31136a11986bb00da58b7
kebasyaty <kebasyaty@gmail.com>
2024-01-22 16:29:48 +0200

    add verify_password method

--- src/crymon/paladins/paladins.cr
+++ src/crymon/paladins/paladins.cr
@@ -1,8 +1,10 @@
 require "./caching"
 require "./check"
+require "./password"
 
 # Paladins - Model instance methods.
 module Crymon::Paladins
   include Crymon::Paladins::Caching
   include Crymon::Paladins::Check
+  include Crymon::Paladins::Password
 end
---
+++ src/crymon/paladins/password.cr
@@ -0,0 +1,15 @@
+module Crymon::Paladins::Password
+  def verify_password(password : String, field_name : String = "password") : Bool
+    if doc_id = self.get_object_id
+      # Get collection for current model.
+      collection : Mongo::Collection = Crymon::Globals.cache_mongo_database.not_nil![
+        @@meta.not_nil![:collection_name]]
+      # Get password hash.
+      password_hash : String = Crypto::Bcrypt::Password.create(password).to_s
+      return !collection.find_one({"_id" => doc_id, field_name => password_hash}).nil?
+    end
+    raise "Panic - Model: `#{@@meta.not_nil![:model_name]}` > " +
+          "Field: `#{field_name}` | Method: `verify_password` => " +
+          "Cannot get document ID - Hash field is empty."
+  end
+end
