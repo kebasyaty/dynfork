e6f276f75c9dcded632987c5dc0ba130443707b2
kebasyaty <kebasyaty@gmail.com>
2024-01-23 16:04:11 +0200

    update erreos

--- src/crymon/errors/meta.cr
+++ src/crymon/errors/meta.cr
@@ -4,22 +4,55 @@ require "./root"
 module Crymon::Errors::Meta
   # Missing parameter for Metadata.
   class ParameterMissing < Crymon::Errors::CrymonException
-    def initialize(model_name : String, parameter_name : String)
-      super("Model: `#{model_name}` => Missing `#{parameter_name}` parameter for Meta.")
+    def initialize(
+      model_name : String,
+      parameter_name : String
+    )
+      super(
+        "Model: `#{model_name}` => " +
+        "Missing `#{parameter_name}` parameter for Meta."
+      )
+    end
+  end
+
+  # Invalid parameter name.
+  class InvalidParamName < Crymon::Errors::CrymonException
+    def initialize(
+      model_name : String,
+      parameter_name : String
+    )
+      super(
+        "Model: `#{model_name}` > Meta parameter: #{parameter_name} => " +
+        "Invalid model parameter name."
+      )
     end
   end
 
   # The allowed number of characters in the metadata parameters has been exceeded.
   class ParamExcessChars < Crymon::Errors::CrymonException
-    def initialize(model_name : String, parameter_name : String, limit_size : UInt32)
-      super("Model: `#{model_name}` > Meta parameter: `#{parameter_name}` => The line size of #{limit_size} characters has been exceeded.")
+    def initialize(
+      model_name : String,
+      parameter_name : String,
+      limit_size : UInt32
+    )
+      super(
+        "Model: `#{model_name}` > Meta parameter: `#{parameter_name}` => " +
+        "The line size of #{limit_size} characters has been exceeded."
+      )
     end
   end
 
   # The metadata parameter fails regular expression validation.
   class ParamRegexFails < Crymon::Errors::CrymonException
-    def initialize(model_name : String, parameter_name : String, regex_str : String)
-      super("Model: `#{model_name}` > Meta parameter: `#{parameter_name}` => Regular expression check fails: #{regex_str}.")
+    def initialize(
+      model_name : String,
+      parameter_name : String,
+      regex_str : String
+    )
+      super(
+        "Model: `#{model_name}` > Meta parameter: `#{parameter_name}` => " +
+        "Regular expression check fails: #{regex_str}."
+      )
     end
   end
 end
