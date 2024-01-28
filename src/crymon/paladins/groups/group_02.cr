1d17c2a610b881985eb4339882d6481a75954346
kebasyaty <kebasyaty@gmail.com>
2024-01-28 11:40:03 +0200

    remove group_2

--- src/crymon/paladins/groups/group_02.cr
+++
@@ -1,9 +0,0 @@
-module Crymon::Paladins::Groups
-  # Validation of SlugField.
-  def group_02(
-    field_ptr : Pointer,
-    is_error_symptom_ptr : Pointer(Bool),
-    is_updated : Bool
-  )
-  end
-end
