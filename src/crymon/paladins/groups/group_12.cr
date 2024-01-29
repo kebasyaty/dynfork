0677a6b43bafea0a23bec951b18d3d1f3834b0ce
kebasyaty <kebasyaty@gmail.com>
2024-01-29 19:41:12 +0200

    update group_12

--- src/crymon/paladins/groups/group_12.cr
+++ src/crymon/paladins/groups/group_12.cr
@@ -2,8 +2,12 @@ module Crymon::Paladins::Groups
   # Validation of fields of type BoolField.
   def group_12(
     field_ptr : Pointer,
-    is_error_symptom_ptr? : Pointer(Bool),
-    is_updated? : Bool
+    is_save? : Bool,
+    result_bson_ptr : Pointer(BSON)
   )
+    current_value : Bool = false
+    (current_value = true) if field_ptr.value.value || field_ptr.value.default
+    # Insert result.
+    (result_bson_ptr.value[field_ptr.value.name] = current_value) if is_save?
   end
 end
