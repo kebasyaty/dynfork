758c0b6b70199aaeec9ce8a3d1a7055bad390fd9
kebasyaty <kebasyaty@gmail.com>
2024-03-01 14:20:31 +0200

    update

---
+++ spec/dynfork/globals/types_spec.cr
@@ -0,0 +1,19 @@
+require "../../spec_helper"
+
+describe DynFork::Globals do
+  describe "FileData" do
+    describe ".new" do
+      it "=> create instance", tags: "global_types" do
+        # ...
+      end
+    end
+  end
+
+  describe "ImageData" do
+    describe ".new" do
+      it "=> create instance", tags: "global_types" do
+        # ...
+      end
+    end
+  end
+end
