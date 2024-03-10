35e6cbe89113cbc7d5cdc3264aa01d4ae074a3d4
kebasyaty <kebasyaty@gmail.com>
2024-03-10 18:03:42 +0200

    fix test

--- spec/dependencies/validator_spec.cr
+++ spec/dependencies/validator_spec.cr
@@ -48,26 +48,26 @@ describe "Crystal Validator" do
 
   it "=> testing color", tags: "validator" do
     # Negative:
-    Valid.color?("").should be_false
-    Valid.color?("#f2ewq").should be_false
+    Valid.color_code?("").should be_false
+    Valid.color_code?("#f2ewq").should be_false
     # Positive:
-    Valid.color?("#fff").should be_true
-    Valid.color?("#f2f2f2").should be_true
-    Valid.color?("#F2F2F2").should be_true
-    Valid.color?("#00000000").should be_true
-    Valid.color?("rgb(255,0,24)").should be_true
-    Valid.color?("rgb(255, 0, 24)").should be_true
-    Valid.color?("rgba(255, 0, 24, .5)").should be_true
-    Valid.color?("rgba(#fff, .5)").should be_true
-    Valid.color?("rgba(#fff,.5)").should be_true
-    Valid.color?("rgba(#FFF, .5)").should be_true
-    Valid.color?("rgba(#FFF,.5)").should be_true
-    Valid.color?("hsl(120, 100%, 50%)").should be_true
-    Valid.color?("hsl(120,100%,50%)").should be_true
-    Valid.color?("hsla(170, 23%, 25%, 0.2)").should be_true
-    Valid.color?("hsla(170,23%,25%,0.2)").should be_true
-    Valid.color?("0x00ffff").should be_true
-    Valid.color?("0x00FFFF").should be_true
+    Valid.color_code?("#fff").should be_true
+    Valid.color_code?("#f2f2f2").should be_true
+    Valid.color_code?("#F2F2F2").should be_true
+    Valid.color_code?("#00000000").should be_true
+    Valid.color_code?("rgb(255,0,24)").should be_true
+    Valid.color_code?("rgb(255, 0, 24)").should be_true
+    Valid.color_code?("rgba(255, 0, 24, .5)").should be_true
+    Valid.color_code?("rgba(#fff, .5)").should be_true
+    Valid.color_code?("rgba(#fff,.5)").should be_true
+    Valid.color_code?("rgba(#FFF, .5)").should be_true
+    Valid.color_code?("rgba(#FFF,.5)").should be_true
+    Valid.color_code?("hsl(120, 100%, 50%)").should be_true
+    Valid.color_code?("hsl(120,100%,50%)").should be_true
+    Valid.color_code?("hsla(170, 23%, 25%, 0.2)").should be_true
+    Valid.color_code?("hsla(170,23%,25%,0.2)").should be_true
+    Valid.color_code?("0x00ffff").should be_true
+    Valid.color_code?("0x00FFFF").should be_true
   end
 
   it "=> testing json", tags: "validator" do
