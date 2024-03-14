7dbfbf6686d02f593246cbfb7c5b18446e2f75db
kebasyaty <kebasyaty@gmail.com>
2024-03-14 14:01:23 +0200

    fix test

--- spec/dynfork/model/check/full_default_spec.cr
+++ spec/dynfork/model/check/full_default_spec.cr
@@ -39,76 +39,76 @@ describe DynFork::Model do
       data.empty?.should be_true
       #
       # Param `value`
-      m.url.value.should be_nil
-      m.text.value.should be_nil
-      m.phone.value.should be_nil
-      m.password.value.should be_nil
-      m.ip.value.should be_nil
-      m.hash.value.should be_nil
-      m.email.value.should be_nil
-      m.color.value.should be_nil
+      m.url.value?.should be_nil
+      m.text.value?.should be_nil
+      m.phone.value?.should be_nil
+      m.password.value?.should be_nil
+      m.ip.value?.should be_nil
+      m.hash.value?.should be_nil
+      m.email.value?.should be_nil
+      m.color.value?.should be_nil
       #
-      m.date.value.should be_nil
-      m.datetime.value.should be_nil
+      m.date.value?.should be_nil
+      m.datetime.value?.should be_nil
       #
-      m.choice_text.value.should be_nil
-      m.choice_text_mult.value.should be_nil
-      m.choice_text_dyn.value.should be_nil
-      m.choice_text_mult_dyn.value.should be_nil
+      m.choice_text.value?.should be_nil
+      m.choice_text_mult.value?.should be_nil
+      m.choice_text_dyn.value?.should be_nil
+      m.choice_text_mult_dyn.value?.should be_nil
       #
-      m.choice_i64.value.should be_nil
-      m.choice_i64_mult.value.should be_nil
-      m.choice_i64_dyn.value.should be_nil
-      m.choice_i64_mult_dyn.value.should be_nil
+      m.choice_i64.value?.should be_nil
+      m.choice_i64_mult.value?.should be_nil
+      m.choice_i64_dyn.value?.should be_nil
+      m.choice_i64_mult_dyn.value?.should be_nil
       #
-      m.choice_f64.value.should be_nil
-      m.choice_f64_mult.value.should be_nil
-      m.choice_f64_dyn.value.should be_nil
-      m.choice_f64_mult_dyn.value.should be_nil
+      m.choice_f64.value?.should be_nil
+      m.choice_f64_mult.value?.should be_nil
+      m.choice_f64_dyn.value?.should be_nil
+      m.choice_f64_mult_dyn.value?.should be_nil
       #
-      m.file.value.should be_nil
-      m.image.value.should be_nil
+      m.file.value?.should be_nil
+      m.image.value?.should be_nil
       #
-      m.i64.value.should be_nil
-      m.f64.value.should be_nil
+      m.i64.value?.should be_nil
+      m.f64.value?.should be_nil
       #
-      m.bool.value.should be_nil
+      m.bool.value?.should be_nil
       #
       # Param `default`
-      m.url.default.should be_nil
-      m.text.default.should be_nil
-      m.phone.default.should be_nil
-      m.password.default.should be_nil
-      m.ip.default.should be_nil
-      m.hash.default.should be_nil
-      m.email.default.should be_nil
-      m.color.default.should be_nil
+      m.url.default?.should be_nil
+      m.text.default?.should be_nil
+      m.phone.default?.should be_nil
+      m.password.default?.should be_nil
+      m.ip.default?.should be_nil
+      m.hash.default?.should be_nil
+      m.email.default?.should be_nil
+      m.color.default?.should be_nil
       #
-      m.date.default.should be_nil
-      m.datetime.default.should be_nil
+      m.date.default?.should be_nil
+      m.datetime.default?.should be_nil
       #
-      m.choice_text.default.should be_nil
-      m.choice_text_mult.default.should be_nil
-      m.choice_text_dyn.default.should be_nil
-      m.choice_text_mult_dyn.default.should be_nil
+      m.choice_text.default?.should be_nil
+      m.choice_text_mult.default?.should be_nil
+      m.choice_text_dyn.default?.should be_nil
+      m.choice_text_mult_dyn.default?.should be_nil
       #
-      m.choice_i64.default.should be_nil
-      m.choice_i64_mult.default.should be_nil
-      m.choice_i64_dyn.default.should be_nil
-      m.choice_i64_mult_dyn.default.should be_nil
+      m.choice_i64.default?.should be_nil
+      m.choice_i64_mult.default?.should be_nil
+      m.choice_i64_dyn.default?.should be_nil
+      m.choice_i64_mult_dyn.default?.should be_nil
       #
-      m.choice_f64.default.should be_nil
-      m.choice_f64_mult.default.should be_nil
-      m.choice_f64_dyn.default.should be_nil
-      m.choice_f64_mult_dyn.default.should be_nil
+      m.choice_f64.default?.should be_nil
+      m.choice_f64_mult.default?.should be_nil
+      m.choice_f64_dyn.default?.should be_nil
+      m.choice_f64_mult_dyn.default?.should be_nil
       #
-      m.file.default.should be_nil
-      m.image.default.should be_nil
+      m.file.default?.should be_nil
+      m.image.default?.should be_nil
       #
-      m.i64.default.should be_nil
-      m.f64.default.should be_nil
+      m.i64.default?.should be_nil
+      m.f64.default?.should be_nil
       #
-      m.bool.default.should be_false
+      m.bool.default?.should be_false
       # ------------------------------------------------------------------------
       #
       # Delete database after test.
