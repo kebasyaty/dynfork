1981a477c756632216867e1898bc16e4fad3c536
kebasyaty <kebasyaty@gmail.com>
2024-03-16 16:54:14 +0200

    fix comments

--- src/dynfork/model.cr
+++ src/dynfork/model.cr
@@ -21,39 +21,39 @@ module DynFork
   # )]
   # struct User < DynFork::Model
   #   getter username = DynFork::Fields::TextField.new(
-  #     "label": "Username",
-  #     "maxlength": 150,
-  #     "minlength": 1,
-  #     "regex": "^[a-zA-Z0-9_@.+]$",
-  #     "regex_err_msg": "Allowed chars: a-z A-Z 0-9 _ @ . +",
-  #     "required": true,
-  #     "unique": true
+  #     label: "Username",
+  #     maxlength: 150,
+  #     minlength: 1,
+  #     regex: "^[a-zA-Z0-9_@.+]$",
+  #     regex_err_msg: "Allowed chars: a-z A-Z 0-9 _ @ . +",
+  #     required: true,
+  #     unique: true
   #   )
   #   getter email = DynFork::Fields::EmailField.new(
-  #     "label": "E-mail",
-  #     "maxlength": 320,
-  #     "required": true,
-  #     "unique": true
+  #     label: "E-mail",
+  #     maxlength: 320,
+  #     required: true,
+  #     unique: true
   #   )
   #   getter birthday = DynFork::Fields::DateField.new(
-  #     "label": "Birthday",
+  #     label: "Birthday",
   #   )
   #   getter slug = DynFork::Fields::SlugField.new(
-  #     "label": "Slug",
-  #     "slug_sources": ["hash", "username"]
+  #     label: "Slug",
+  #     slug_sources: ["hash", "username"]
   #   )
   #   getter password = DynFork::Fields::PasswordField.new(
-  #     "label": "Password",
+  #     label: "Password",
   #   )
   #   # Do not save the value of this field to the database.
   #   # This field is for verification purposes only.
   #   getter confirm_password = DynFork::Fields::PasswordField.new(
-  #     "label": "Confirm password",
-  #     "ignored": true
+  #     label: "Confirm password",
+  #     ignored: true
   #   )
   #   getter active: DynFork::Fields::BoolField.new(
-  #     "label": "is active?",
-  #     "default": true
+  #     label: "is active?",
+  #     default: true
   #   )
   # end
   # ```
