acac474fdf18592c61e955d43f2b8f7932dbe7e4
kebasyaty <kebasyaty@gmail.com>
2024-03-16 09:55:48 +0200

    update test

--- spec/dynfork/model/check/value_no_nil_spec.cr
+++ spec/dynfork/model/check/value_no_nil_spec.cr
@@ -6,7 +6,7 @@ describe DynFork::Model do
       # Init data for test.
       #
       # To generate a key (This is not an advertisement): https://randompasswordgen.com/
-      unique_app_key = "I96wl3g9f59vyW5e"
+      unique_app_key = "E2ep4e3UPkWs84GO"
       database_name = "test_#{unique_app_key}"
       mongo_uri = "mongodb://localhost:27017"
 
