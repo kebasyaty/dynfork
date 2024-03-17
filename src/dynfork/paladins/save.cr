a8106c04d5f10c42fac97cf4f33428f3d9dcadf3
kebasyaty <kebasyaty@gmail.com>
2024-03-17 11:04:43 +0200

    update save method

--- src/dynfork/paladins/save.cr
+++ src/dynfork/paladins/save.cr
@@ -4,6 +4,13 @@ module DynFork::Paladins::Save
   # <br>
   # This method pre-uses the _check_ method.
   def save
-    # ...
+    # Get collection.
+    collection : Mongo::Collection = DynFork::Globals.cache_mongo_database[
+      @@meta.not_nil![:collection_name]]
+    # Check and get output data.
+    output_data : DynFork::Globals::OutputData = m.check(
+      collection_ptr: pointerof(collection),
+      save?: true
+    )
   end
 end
