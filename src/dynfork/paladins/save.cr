fa11ec5f80cce37b8f12ec3cce01544ea13bcae7
kebasyaty <kebasyaty@gmail.com>
2024-03-17 10:57:43 +0200

    update comments

--- src/dynfork/paladins/save.cr
+++ src/dynfork/paladins/save.cr
@@ -1,6 +1,8 @@
 # Creating and updating documents in the database.
 module DynFork::Paladins::Save
-  # ???
+  # Creating and updating documents in the database.
+  # <br>
+  # This method pre-uses the _check_ method.
   def save
     # ...
   end
