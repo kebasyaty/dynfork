c956aed07587492aa74a99fd42019a91d5fa4c20
kebasyaty <kebasyaty@gmail.com>
2024-03-19 07:46:33 +0200

    update refrash_fields()

--- src/dynfork/paladins/check_plus.cr
+++ src/dynfork/paladins/check_plus.cr
@@ -146,19 +146,19 @@ module DynFork::Paladins::CheckPlus
             # | ChoiceI64MultDynField | ChoiceF64MultDynField
             if field_type.includes?("Text")
               if @{{ field }}.field_type.includes?("Mult")
-                @{{ field }}.refrash_val_arr_str()
+                @{{ field }}.refrash_val_arr_str(Array(String).from_bson(value.as(BSON)))
               else
                 @{{ field }}.refrash_val_str(value.as(String))
               end
             elsif field_type.includes?("I64")
               if @{{ field }}.field_type.includes?("Mult")
-                # ...
+                @{{ field }}.refrash_val_arr_i64(Array(Int64).from_bson(value.as(BSON)))
               else
                 @{{ field }}.refrash_val_i64(value.as(Int64))
               end
             elsif field_type.includes?("F64")
               if @{{ field }}.field_type.includes?("Mult")
-                # ...
+                @{{ field }}.refrash_val_arr_str(Array(Float64).from_bson(value.as(BSON)))
               else
                 @{{ field }}.refrash_val_f64(value.as(Float64))
               end
