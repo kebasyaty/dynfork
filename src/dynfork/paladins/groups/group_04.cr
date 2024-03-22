373ea8c509030c301b49edf9a0823696954ff962
kebasyaty <kebasyaty@gmail.com>
2024-03-22 19:34:04 +0200

    fix

--- src/dynfork/paladins/groups/group_04.cr
+++ src/dynfork/paladins/groups/group_04.cr
@@ -29,6 +29,7 @@ module DynFork::Paladins::Groups
     if !update? && current_value.nil?
       if default = field_ptr.value.default?
         field_ptr.value.path_to_file(default.to_s)
+        current_value = field_ptr.value.value
       end
     end
 
