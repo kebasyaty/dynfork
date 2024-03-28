c7a593320d7e8008abf141b1b50c5d62f05e16a0
kebasyaty <kebasyaty@gmail.com>
2024-03-28 08:00:06 +0200

    update tests

--- spec/dynfork/model/save/update/value_no_nil_spec.cr
+++ spec/dynfork/model/save/update/value_no_nil_spec.cr
@@ -73,11 +73,7 @@ describe DynFork::Model do
       flag.should be_true
       m.color.value?.should eq("#ff0000")
       #
-      # Get the collection for the current model.
-      collection : Mongo::Collection = DynFork::Globals.cache_mongo_database[
-        Spec::Data::ValueNoNil.meta[:collection_name]]
-      #
-      collection.count_documents.should eq(1)
+      m.count_documents.should eq(1)
       # Update
       m.url.value = "https://randompasswordgen.com/"
       m.phone.value = "+18004444455"
@@ -104,7 +100,7 @@ describe DynFork::Model do
       flag.should be_true
       #
       #
-      collection.count_documents.should eq(1)
+      m.count_documents.should eq(1)
       #
       # Param `value`
       m.hash.value.empty?.should be_false
