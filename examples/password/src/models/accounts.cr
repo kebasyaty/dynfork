44620d382efe47b6410fb3009d8005530f188892
kebasyaty <kebasyaty@gmail.com>
2024-03-29 18:26:06 +0200

    update example

--- examples/password/src/models/accounts.cr
+++ examples/password/src/models/accounts.cr
@@ -4,6 +4,7 @@ module Models::Accounts
     getter username = DynFork::Fields::TextField.new
     # WARNING: Default regular expression: /^[-._!"`'#%&,:;<>=@{}~\$\(\)\*\+\/\\\?\[\]\^\|a-zA-Z0-9]+$/
     # WARNING: Valid characters by default: a-z A-Z 0-9 - . _ ! " ` ' # % & , : ; < > = @ { } ~ $ ( ) * + / \ ? [ ] ^ |
+    # WARNING: Default number of characters: max = 256, min = 8
     getter password = DynFork::Fields::PasswordField.new
   end
 end
