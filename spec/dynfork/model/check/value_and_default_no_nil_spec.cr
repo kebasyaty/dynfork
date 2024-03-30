8347fe120311c6832f7f4ecddf717df39955c520
kebasyaty <kebasyaty@gmail.com>
2024-03-30 15:22:16 +0200

    fix

--- spec/dynfork/model/check/value_and_default_no_nil_spec.cr
+++ spec/dynfork/model/check/value_and_default_no_nil_spec.cr
@@ -68,7 +68,7 @@ describe DynFork::Model do
       #
       # Get the collection for the current model.
       collection : Mongo::Collection = DynFork::Globals.cache_mongo_database[
-        Spec::Data::ValueNoNil.meta[:collection_name]]
+        Spec::Data::ValueAndDefaultNoNil.meta[:collection_name]]
       #
       output_data : DynFork::Globals::OutputData = m.check(pointerof(collection))
       valid = output_data.valid?
