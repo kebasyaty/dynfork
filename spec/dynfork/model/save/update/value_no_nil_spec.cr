2c6817e220f05d1aa01717d5ad45275a3fd25b00
kebasyaty <kebasyaty@gmail.com>
2024-03-31 08:53:45 +0300

    fix

--- spec/dynfork/model/save/update/value_no_nil_spec.cr
+++ spec/dynfork/model/save/update/value_no_nil_spec.cr
@@ -89,8 +89,8 @@ describe DynFork::Model do
       m.choice_i64_mult.value = [5_i64, 10_i64]
       m.choice_f64.value = 5.25
       m.choice_f64_mult.value = [5.0, 5.25]
-      m.file.path_to_file(delete: true)
-      m.image.path_to_file("pictures/pluto_3.webp")
+      m.file.from_path(delete: true)
+      m.image.from_path("pictures/pluto_3.webp")
       m.i64.value = 20_i64
       m.f64.value = 20.2
       m.bool.value = false
