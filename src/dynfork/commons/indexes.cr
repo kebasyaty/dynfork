ceeb35c8e2e075df89de23c919897ed6651239e0
kebasyaty <kebasyaty@gmail.com>
2024-04-10 16:30:32 +0300

    fix

--- src/dynfork/commons/indexes.cr
+++ src/dynfork/commons/indexes.cr
@@ -1,5 +1,31 @@
 # ???
 module DynFork::Commons::Indexes
   extend self
-  # ...
+
+  # This is a convenience method for creating a single index.
+  # <br>
+  # See #create_indexes:
+  # <br>
+  # https://elbywan.github.io/cryomongo/Mongo/Collection.html#create_indexes%28models%3AArray%2C%2A%2Ccommit_quorum%3AInt32%7CString%3F%3Dnil%2Cmax_time_ms%3AInt64%3F%3Dnil%2Cwrite_concern%3AWriteConcern%3F%3Dnil%2Csession%3ASession%3A%3AClientSession%3F%3Dnil%29%3ACommands%3A%3ACreateIndexes%3A%3AResult%3F-instance-method
+  def create_index(
+    keys,
+    options = NamedTuple.new,
+    commit_quorum : Int32 | String? = nil,
+    max_time_ms : Int64? = nil,
+    write_concern : WriteConcern? = nil,
+    session : Session::ClientSession? = nil
+  ) : Commands::CreateIndexes::Result?
+    # Get collection for current model.
+    collection : Mongo::Collection = DynFork::Globals.cache_mongo_database[
+      @@meta.not_nil![:collection_name]]
+    #
+    collection.create_index(
+      keys: keys,
+      options: options,
+      commit_quorum: commit_quorum,
+      max_time_ms: max_time_ms,
+      write_concern: write_concern,
+      session: session,
+    )
+  end
 end
