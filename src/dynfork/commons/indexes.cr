8965ff96ac63b40207f3b44634bf82d8cddc842e
kebasyaty <kebasyaty@gmail.com>
2024-04-10 16:40:53 +0300

    fix drop_index()

--- src/dynfork/commons/indexes.cr
+++ src/dynfork/commons/indexes.cr
@@ -4,9 +4,7 @@ module DynFork::Commons::Indexes
 
   # This is a convenience method for creating a single index.
   # <br>
-  # See #create_indexes:
-  # <br>
-  # https://elbywan.github.io/cryomongo/Mongo/Collection.html#create_indexes%28models%3AArray%2C%2A%2Ccommit_quorum%3AInt32%7CString%3F%3Dnil%2Cmax_time_ms%3AInt64%3F%3Dnil%2Cwrite_concern%3AWriteConcern%3F%3Dnil%2Csession%3ASession%3A%3AClientSession%3F%3Dnil%29%3ACommands%3A%3ACreateIndexes%3A%3AResult%3F-instance-method
+  # See: #create_indexes.
   def create_index(
     keys,
     options = NamedTuple.new,
@@ -52,4 +50,24 @@ module DynFork::Commons::Indexes
       session: session,
     )
   end
+
+  # Drops a single index from the collection by the index name.
+  # <br>
+  # See: #drop_indexes.
+  def drop_index(
+    name : String,
+    max_time_ms : Int64? = nil,
+    write_concern : WriteConcern? = nil,
+    session : Session::ClientSession? = nil
+  ) : Commands::Common::BaseResult?
+    # Get collection for current model.
+    collection : Mongo::Collection = DynFork::Globals.cache_mongo_database[
+      @@meta.not_nil![:collection_name]]
+    #
+    collection.drop_index(
+    name: name,
+    max_time_ms: max_time_ms,
+    write_concern: write_concern,
+    session: session,
+  )
 end
