e6f0d4b2b0f9a4e1390c0d22b81a15142d8a6fd0
kebasyaty <kebasyaty@gmail.com>
2024-04-10 19:09:29 +0300

    update find_one_to_instance()

--- src/dynfork/commons/one.cr
+++ src/dynfork/commons/one.cr
@@ -31,25 +31,29 @@ module DynFork::Commons::QOne
     collection : Mongo::Collection = DynFork::Globals.cache_mongo_database[
       @@meta.not_nil![:collection_name]]
     #
-    collection.find_one_to_instance(
-      filter: filter,
-      sort: sort,
-      projection: projection,
-      hint: hint,
-      skip: skip,
-      comment: comment,
-      max_time_ms: max_time_ms,
-      read_concern: read_concern,
-      max: max,
-      min: min,
-      return_key: return_key,
-      show_record_id: show_record_id,
-      oplog_replay: oplog_replay,
-      no_cursor_timeout: no_cursor_timeout,
-      allow_partial_results: allow_partial_results,
-      collation: collation,
-      read_preference: read_preference,
-      session: session,
-    )
+    if doc : BSON? = collection.find_one_to_instance(
+         filter: filter,
+         sort: sort,
+         projection: projection,
+         hint: hint,
+         skip: skip,
+         comment: comment,
+         max_time_ms: max_time_ms,
+         read_concern: read_concern,
+         max: max,
+         min: min,
+         return_key: return_key,
+         show_record_id: show_record_id,
+         oplog_replay: oplog_replay,
+         no_cursor_timeout: no_cursor_timeout,
+         allow_partial_results: allow_partial_results,
+         collation: collation,
+         read_preference: read_preference,
+         session: session,
+       )
+      return self.new.refrash_fields doc
+    end
+    #
+    nil
   end
 end
