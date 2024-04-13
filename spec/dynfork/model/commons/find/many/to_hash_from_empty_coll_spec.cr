fd80a00740de8c65b7b2c294fcd22ea387c22ae7
kebasyaty <kebasyaty@gmail.com>
2024-04-13 19:36:21 +0300

    fix

---
+++ spec/dynfork/model/commons/find/many/to_hash_from_empty_coll_spec.cr
@@ -0,0 +1,42 @@
+require "../../../../../spec_helper"
+
+describe DynFork::Model do
+  describe ".find_many_to_hash_list" do
+    it "=> from empty collection", tags: "find_many" do
+      # Init data for test.
+      #
+      # To generate a key (This is not an advertisement): https://randompasswordgen.com/
+      unique_app_key = "r4cAr8z2XF6S3h0x"
+      database_name = "test_#{unique_app_key}"
+      mongo_uri = "mongodb://localhost:27017"
+
+      # Delete database before test.
+      # (if the test fails)
+      Spec::Support.delete_test_db(
+        Mongo::Client.new(mongo_uri)[database_name])
+
+      # Run migration.
+      DynFork::Migration::Monitor.new(
+        "app_name": "AppName",
+        "unique_app_key": unique_app_key,
+        "database_name": database_name,
+        "mongo_uri": mongo_uri,
+        "model_list": {
+          Spec::Data::FullDefault,
+        }
+      ).migrat
+      #
+      # HELLISH BURN
+      # ------------------------------------------------------------------------
+      arr : Array(Hash(String, DynFork::Globals::ValueTypes)) = Spec::Data::FullDefault.find_many_to_hash_list
+      arr.empty?.should be_true
+      # ------------------------------------------------------------------------
+      #
+      # Delete database after test.
+      Spec::Support.delete_test_db(
+        DynFork::Globals.cache_mongo_database)
+      #
+      DynFork::Globals.cache_mongo_client.close
+    end
+  end
+end
--- spec/dynfork/model/commons/find/many/to_json_from_empty_coll_spec.cr
+++ spec/dynfork/model/commons/find/many/to_json_from_empty_coll_spec.cr
@@ -1,42 +1,42 @@
-require "../../../../../spec_helper"
+# require "../../../../../spec_helper"
 
-describe DynFork::Model do
-  describe ".find_many_to_json" do
-    it "=> from empty collection", tags: "find_many" do
-      # Init data for test.
-      #
-      # To generate a key (This is not an advertisement): https://randompasswordgen.com/
-      unique_app_key = "23316i6H423N69L3"
-      database_name = "test_#{unique_app_key}"
-      mongo_uri = "mongodb://localhost:27017"
+# describe DynFork::Model do
+#   describe ".find_many_to_json" do
+#     it "=> from empty collection", tags: "find_many" do
+#       # Init data for test.
+#       #
+#       # To generate a key (This is not an advertisement): https://randompasswordgen.com/
+#       unique_app_key = "23316i6H423N69L3"
+#       database_name = "test_#{unique_app_key}"
+#       mongo_uri = "mongodb://localhost:27017"
 
-      # Delete database before test.
-      # (if the test fails)
-      Spec::Support.delete_test_db(
-        Mongo::Client.new(mongo_uri)[database_name])
+#       # Delete database before test.
+#       # (if the test fails)
+#       Spec::Support.delete_test_db(
+#         Mongo::Client.new(mongo_uri)[database_name])
 
-      # Run migration.
-      DynFork::Migration::Monitor.new(
-        "app_name": "AppName",
-        "unique_app_key": unique_app_key,
-        "database_name": database_name,
-        "mongo_uri": mongo_uri,
-        "model_list": {
-          Spec::Data::FullDefault,
-        }
-      ).migrat
-      #
-      # HELLISH BURN
-      # ------------------------------------------------------------------------
-      json : String = Spec::Data::FullDefault.find_many_to_json
-      json.empty?.should be_true
-      # ------------------------------------------------------------------------
-      #
-      # Delete database after test.
-      Spec::Support.delete_test_db(
-        DynFork::Globals.cache_mongo_database)
-      #
-      DynFork::Globals.cache_mongo_client.close
-    end
-  end
-end
+#       # Run migration.
+#       DynFork::Migration::Monitor.new(
+#         "app_name": "AppName",
+#         "unique_app_key": unique_app_key,
+#         "database_name": database_name,
+#         "mongo_uri": mongo_uri,
+#         "model_list": {
+#           Spec::Data::FullDefault,
+#         }
+#       ).migrat
+#       #
+#       # HELLISH BURN
+#       # ------------------------------------------------------------------------
+#       json : String = Spec::Data::FullDefault.find_many_to_json
+#       json.empty?.should be_true
+#       # ------------------------------------------------------------------------
+#       #
+#       # Delete database after test.
+#       Spec::Support.delete_test_db(
+#         DynFork::Globals.cache_mongo_database)
+#       #
+#       DynFork::Globals.cache_mongo_client.close
+#     end
+#   end
+# end
