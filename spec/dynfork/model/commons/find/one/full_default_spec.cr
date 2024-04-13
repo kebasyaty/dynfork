fe45654dcc44205eb365b75ad98f01aa257a27bf
kebasyaty <kebasyaty@gmail.com>
2024-04-13 15:06:00 +0300

    update test

--- spec/dynfork/model/commons/find/one/full_default_spec.cr
+++ spec/dynfork/model/commons/find/one/full_default_spec.cr
@@ -39,4 +39,43 @@ describe DynFork::Model do
       DynFork::Globals.cache_mongo_client.close
     end
   end
+
+  describe ".find_one_to_json" do
+    it "=> from an empty collection", tags: "find_one" do
+      # Init data for test.
+      #
+      # To generate a key (This is not an advertisement): https://randompasswordgen.com/
+      unique_app_key = "ik83sDFW1Gp916L3"
+      database_name = "test_#{unique_app_key}"
+      mongo_uri = "mongodb://localhost:27017"
+
+      # Delete database before test.
+      # (if the test fails)
+      Spec::Support.delete_test_db(
+        Mongo::Client.new(mongo_uri)[database_name])
+
+      # Run migration.
+      DynFork::Migration::Monitor.new(
+        "app_name": "AppName",
+        "unique_app_key": unique_app_key,
+        "database_name": database_name,
+        "mongo_uri": mongo_uri,
+        "model_list": {
+          Spec::Data::FullDefault,
+        }
+      ).migrat
+      #
+      # HELLISH BURN
+      # ------------------------------------------------------------------------
+      instance = Spec::Data::FullDefault.find_one_to_json
+      instance.should be_nil
+      # ------------------------------------------------------------------------
+      #
+      # Delete database after test.
+      Spec::Support.delete_test_db(
+        DynFork::Globals.cache_mongo_database)
+      #
+      DynFork::Globals.cache_mongo_client.close
+    end
+  end
 end
