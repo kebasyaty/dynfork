d1598d8d88ff6e158389397dc654e4c969c9cadb
kebasyaty <kebasyaty@gmail.com>
2024-04-13 17:19:23 +0300

    fix

--- spec/dynfork/model/commons/find/one/full_default_spec.cr
+++ spec/dynfork/model/commons/find/one/full_default_spec.cr
@@ -1,46 +1,7 @@
 require "../../../../../spec_helper"
 
 describe DynFork::Model do
-  describe ".find_one_to_instance" do
-    it "=> from an empty collection", tags: "find_one" do
-      # Init data for test.
-      #
-      # To generate a key (This is not an advertisement): https://randompasswordgen.com/
-      unique_app_key = "2tx8799Br0710XDM"
-      database_name = "test_#{unique_app_key}"
-      mongo_uri = "mongodb://localhost:27017"
-
-      # Delete database before test.
-      # (if the test fails)
-      Spec::Support.delete_test_db(
-        Mongo::Client.new(mongo_uri)[database_name])
-
-      # Run migration.
-      DynFork::Migration::Monitor.new(
-        "app_name": "AppName",
-        "unique_app_key": unique_app_key,
-        "database_name": database_name,
-        "mongo_uri": mongo_uri,
-        "model_list": {
-          Spec::Data::FullDefault,
-        }
-      ).migrat
-      #
-      # HELLISH BURN
-      # ------------------------------------------------------------------------
-      instance = Spec::Data::FullDefault.find_one_to_instance
-      instance.should be_nil
-      # ------------------------------------------------------------------------
-      #
-      # Delete database after test.
-      Spec::Support.delete_test_db(
-        DynFork::Globals.cache_mongo_database)
-      #
-      DynFork::Globals.cache_mongo_client.close
-    end
-  end
-
-  describe ".find_one_to_json" do
+  describe "Find One" do
     it "=> from an empty collection", tags: "find_one" do
       # Init data for test.
       #
@@ -67,8 +28,11 @@ describe DynFork::Model do
       #
       # HELLISH BURN
       # ------------------------------------------------------------------------
+      instance = Spec::Data::FullDefault.find_one_to_instance
+      instance.should be_nil
+      #
       json : String = Spec::Data::FullDefault.find_one_to_json
-      json.should eq ""
+      json.empty?.should be_true
       # ------------------------------------------------------------------------
       #
       # Delete database after test.
