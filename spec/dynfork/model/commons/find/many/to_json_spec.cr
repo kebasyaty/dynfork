181487fb0b3742b9d3b4bb69ddc538cac3d24e75
kebasyaty <kebasyaty@gmail.com>
2024-04-14 15:40:33 +0300

    fix

--- spec/dynfork/model/commons/find/many/to_json_spec.cr
+++ spec/dynfork/model/commons/find/many/to_json_spec.cr
@@ -31,47 +31,47 @@ describe DynFork::Model do
       json : String = Spec::Data::ValueNoNil.find_many_to_json
       json.empty?.should be_true
       #
-      m = Spec::Data::ValueNoNil.new
-      #
-      # Init `value`
-      m.url.value = "https://translate.google.com/"
-      m.text.value = "Some text"
-      m.phone.value = "+18004444444"
-      m.password.value = "7637Kw8#5GTb~]H#"
-      m.ip.value = "126.255.255.255"
-      m.hash2.value = "507c7f79bcf86cd7994f6c0e"
-      m.email.value = "john.smith@example.com"
-      m.color.value = "#ff0000"
-      #
-      m.date.value = "1970-01-01"
-      m.datetime.value = "1970-01-01T00:00:00"
-      #
-      m.choice_text.value = "value"
-      m.choice_text_mult.value = ["value"]
-      m.choice_text_dyn.value?.should be_nil
-      m.choice_text_mult_dyn.value?.should be_nil
-      #
-      m.choice_i64.value = 5_i64
-      m.choice_i64_mult.value = [5_i64]
-      m.choice_i64_dyn.value?.should be_nil
-      m.choice_i64_mult_dyn.value?.should be_nil
-      #
-      m.choice_f64.value = 5.0
-      m.choice_f64_mult.value = [5.0]
-      m.choice_f64_dyn.value?.should be_nil
-      m.choice_f64_mult_dyn.value?.should be_nil
-      #
-      m.file.from_path("assets/media/default/no_doc.odt")
-      m.image.from_path("assets/media/default/no_photo.jpeg")
-      #
-      m.i64.value = 10_i64
-      m.f64.value = 10.2
-      #
-      m.bool.value = true
-      #
-      m.save
-      m = Spec::Data::ValueNoNil.new
-      m.save
+      2.times { |idx|
+        m = Spec::Data::ValueNoNil.new
+        #
+        # Init `value`
+        m.url.value = "https://translate.google.com/"
+        m.text.value = "Some text #{idx}"
+        m.phone.value = "+18004444444"
+        m.password.value = "7637Kw8#5GTb~]H#"
+        m.ip.value = "126.255.255.255"
+        m.hash2.value = "507c7f79bcf86cd7994f6c0e"
+        m.email.value = "john.smith@example.com"
+        m.color.value = "#ff0000"
+        #
+        m.date.value = "1970-01-01"
+        m.datetime.value = "1970-01-01T00:00:00"
+        #
+        m.choice_text.value = "value"
+        m.choice_text_mult.value = ["value"]
+        m.choice_text_dyn.value?.should be_nil
+        m.choice_text_mult_dyn.value?.should be_nil
+        #
+        m.choice_i64.value = 5_i64
+        m.choice_i64_mult.value = [5_i64]
+        m.choice_i64_dyn.value?.should be_nil
+        m.choice_i64_mult_dyn.value?.should be_nil
+        #
+        m.choice_f64.value = 5.0
+        m.choice_f64_mult.value = [5.0]
+        m.choice_f64_dyn.value?.should be_nil
+        m.choice_f64_mult_dyn.value?.should be_nil
+        #
+        m.file.from_path("assets/media/default/no_doc.odt")
+        m.image.from_path("assets/media/default/no_photo.jpeg")
+        #
+        m.i64.value = 10_i64
+        m.f64.value = 10.2
+        #
+        m.bool.value = true
+        #
+        m.save
+      }
       Spec::Data::ValueNoNil.count_documents.should eq(2)
       Spec::Data::ValueNoNil.estimated_document_count.should eq(2)
       #
