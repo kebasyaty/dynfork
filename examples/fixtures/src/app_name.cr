bb027ce1db538912a864653a886e0e68cbaee726
kebasyaty <kebasyaty@gmail.com>
2024-04-18 15:19:07 +0300

    update

---
+++ examples/fixtures/src/app_name.cr
@@ -0,0 +1,25 @@
+require "i18n"
+require "dynfork"
+require "./models/*"
+
+module AppName
+  VERSION = "0.1.0"
+
+  # Initialize locale.
+  # https://github.com/crystal-i18n/i18n
+  I18n.config.loaders << I18n::Loader::YAML.new("config/locales")
+  I18n.config.default_locale = :en
+  I18n.init
+
+  # Run migration.
+  DynFork::Migration::Monitor.new(
+    "app_name": "AppName",
+    "unique_app_key": "0U6r2itxQkF3l1Dx",
+    "mongo_uri": "mongodb://localhost:27017",
+    "model_list": {
+      Models::Admin::SiteSettings,
+    }
+  ).migrat
+
+  puts "Number of documents: #{Models::Admin::SiteSettings.estimated_document_count}" # => 1
+end
