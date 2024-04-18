15edfba4a520bb970ee6c7185f1901060d410411
kebasyaty <kebasyaty@gmail.com>
2024-04-18 11:48:13 +0300

    fix

--- src/dynfork/paladins/tools.cr
+++ src/dynfork/paladins/tools.cr
@@ -386,7 +386,7 @@ module DynFork::Paladins::Tools
       end
     {% end %}
     #
-    unless self.save
+    unless self.valid?
       print "\nFIXTURE: ".colorize.fore(:red).mode(:bold)
       print fixture_path.colorize.fore(:blue).mode(:bold)
       self.print_err
