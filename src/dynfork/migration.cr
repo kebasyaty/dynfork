4e7a3bf713ce41ed20d36e80e7cfbf2a92c2bab4
kebasyaty <kebasyaty@gmail.com>
2024-04-23 16:27:13 +0300

    fix

--- src/dynfork/migration.cr
+++ src/dynfork/migration.cr
@@ -173,11 +173,10 @@ module DynFork::Migration
                   "The number of fields does not match!"
                 )
               end
-              doc_h = doc.to_h
               model_state.field_name_and_type_list.each do |field_name, field_type|
                 if field_type == "PasswordField"
-                  if !(value = doc_h[field_name]?).nil?
-                    data[field_name] = value.not_nil!
+                  if !(value = doc[field_name]?).nil?
+                    data[field_name] = value
                   end
                 end
               end
