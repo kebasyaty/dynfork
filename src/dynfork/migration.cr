98ff659cda8ea2fa363557bb5a028de0841913c3
kebasyaty <kebasyaty@gmail.com>
2024-04-24 08:50:49 +0300

    fix

--- src/dynfork/migration.cr
+++ src/dynfork/migration.cr
@@ -230,8 +230,7 @@ module DynFork::Migration
         model_state.field_name_and_type_list = metadata[:field_name_and_type_list]
         # Update the state of the current Model.
         filter = {"collection_name": model_state.collection_name}
-        update = {"$set": model_state}
-        super_collection.update_one(filter, update)
+        super_collection.replace_one(filter, model_state)
       end
       #
       # ------------------------------------------------------------------------
