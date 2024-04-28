0aa66b8bf017ee044e4f27819c39ef09895fe0af
kebasyaty <kebasyaty@gmail.com>
2024-04-28 14:20:38 +0300

    remove []? method

--- spec/dynfork/model/json_model_spec.cr
+++ spec/dynfork/model/json_model_spec.cr
@@ -37,13 +37,6 @@ describe Spec::Data::FilledModel do
       f.first_name.warning.should eq("")
       f.first_name.errors.should eq(Array(String).new)
       f.first_name.group.should eq(1_u8)
-      f["first_name"]?.should be_true
-      f["age"]?.should be_true
-      f["birthday"]?.should be_true
-      f["hash"]?.should be_true
-      f["created_at"]?.should be_true
-      f["updated_at"]?.should be_true
-      f["???"]?.should be_false
       #
       # I64Field
       f.age.id.should eq("FilledModel--age")
@@ -66,13 +59,6 @@ describe Spec::Data::FilledModel do
       f.age.warning.should eq("")
       f.age.errors.should eq(Array(String).new)
       f.age.group.should eq(6_u8)
-      f["first_name"]?.should be_true
-      f["age"]?.should be_true
-      f["birthday"]?.should be_true
-      f["hash"]?.should be_true
-      f["created_at"]?.should be_true
-      f["updated_at"]?.should be_true
-      f["???"]?.should be_false
       #
       # DateField
       f.birthday.id.should eq("FilledModel--birthday")
@@ -94,13 +80,6 @@ describe Spec::Data::FilledModel do
       f.birthday.warning.should eq("")
       f.birthday.errors.should eq(Array(String).new)
       f.birthday.group.should eq(2_u8)
-      f["first_name"]?.should be_true
-      f["age"]?.should be_true
-      f["birthday"]?.should be_true
-      f["hash"]?.should be_true
-      f["created_at"]?.should be_true
-      f["updated_at"]?.should be_true
-      f["???"]?.should be_false
     end
   end
 end
