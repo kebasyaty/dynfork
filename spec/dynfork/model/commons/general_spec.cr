ca8bcdd26490dcc743d8524b0775984da3f13dd9
kebasyaty <kebasyaty@gmail.com>
2024-05-01 14:29:45 +0300

    update tests

--- spec/dynfork/model/commons/general_spec.cr
+++ spec/dynfork/model/commons/general_spec.cr
@@ -16,10 +16,10 @@ describe DynFork::Commons::QGeneral do
 
     # Run migration.
     DynFork::Migration::Monitor.new(
-      "app_name": "AppName",
-      "unique_app_key": unique_app_key,
-      "database_name": database_name,
-      "mongo_uri": mongo_uri,
+      app_name: "AppName",
+      unique_app_key: unique_app_key,
+      database_name: database_name,
+      mongo_client: Mongo::Client.new(mongo_uri)
     ).migrat
     #
     # HELLISH BURN
