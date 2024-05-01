e123c2a64f6ff1177512670ecde9c78f1a899e0a
kebasyaty <kebasyaty@gmail.com>
2024-05-01 14:50:30 +0300

    change version number

--- src/dynfork.cr
+++ src/dynfork.cr
@@ -51,7 +51,7 @@ require "./dynfork/migration"
 # <br>
 #  DynFork is free software under terms of the Apache Version 2.0 and MIT License.
 module DynFork
-  VERSION = "0.3.1"
+  VERSION = "0.4.0"
 end
 
 module Validator
