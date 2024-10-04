0b0250fe5b6de3c2c55351cc5cfeffd522df6d98
kebasyaty <kebasyaty@gmail.com>
2024-10-04 16:20:36 +0300

    raise number build

--- src/dynfork.cr
+++ src/dynfork.cr
@@ -51,7 +51,7 @@ require "./dynfork/migration"
 # <br>
 #  DynFork is free software under terms of the MIT License.
 module DynFork
-  VERSION = "0.7.23"
+  VERSION = "0.7.24"
 end
 
 module Validator
