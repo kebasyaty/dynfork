04faba6dddbb3c69e1509b7b5cd652958166c99b
kebasyaty <kebasyaty@gmail.com>
2024-11-17 14:33:42 +0200

    Update DynFork::Errors::Cache

--- src/dynfork/errors/cache.cr
+++ src/dynfork/errors/cache.cr
@@ -9,7 +9,7 @@ module DynFork::Errors::Cache
       limit_size : UInt32
     )
       super(
-        "Global settings > Parameter: `#{parameter_name}` => " +
+        "DynFork::Globals > Parameter: `#{parameter_name}` => " +
         "The line size of #{limit_size} characters has been exceeded!"
       )
     end
@@ -22,7 +22,7 @@ module DynFork::Errors::Cache
       regex_str : String
     )
       super(
-        "Global settings > Parameter: `#{parameter_name}` => " +
+        "DynFork::Globals > Parameter: `#{parameter_name}` => " +
         "Regular expression check fails: #{regex_str}."
       )
     end
