d6b529dc0cb6aa6189c0c0669b4e347d040f2d5b
kebasyaty <kebasyaty@gmail.com>
2024-11-22 07:57:59 +0200

    update comment

--- src/dynfork.cr
+++ src/dynfork.cr
@@ -57,9 +57,8 @@ end
 
 module Validator
   # Custom validator for checking color code.
-  # <br>
-  # _Examples: #fff | #f2f2f2 | #f2f2f200 | rgb(255,0,24) | rgba(255,0,24,0.5) |
-  # rgba(#fff,0.5) | hsl(120,100%,50%) | hsla(170,23%,25%,0.2) | 0x00ffff_#
+  # NOTE: **Examples:** _#fff | #f2f2f2 | #f2f2f200 | rgb(255,0,24) |
+  # rgba(255,0,24,0.5) | rgba(#fff,0.5) | hsl(120,100%,50%) | hsla(170,23%,25%,0.2) | 0x00ffff_
   def self.color_code?(value : String) : Bool
     DynFork::Globals.regex[:color_code].matches?(value)
   end
