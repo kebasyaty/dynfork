6e73c28da34ddfdda46e24f0618014086b7b8926
kebasyaty <kebasyaty@gmail.com>
2024-12-02 10:08:09 +0200

    Update comment

--- src/dynfork.cr
+++ src/dynfork.cr
@@ -56,7 +56,7 @@ module DynFork
 end
 
 module Validator
-  # Custom validator for checking color code.
+  # Custom validator to validate the color code string.
   # NOTE: **Examples:** _#fff | #f2f2f2 | #f2f2f200 | rgb(255,0,24) |
   # rgba(255,0,24,0.5) | rgba(#fff,0.5) | hsl(120,100%,50%) | hsla(170,23%,25%,0.2) | 0x00ffff_
   def self.color_code?(value : String) : Bool
