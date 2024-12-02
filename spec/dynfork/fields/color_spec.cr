904ad2da6aec5fbd35835542ef0d512c134116f5
kebasyaty <kebasyaty@gmail.com>
2024-12-02 11:28:29 +0200

    Update test for Color field

--- spec/dynfork/fields/color_spec.cr
+++ spec/dynfork/fields/color_spec.cr
@@ -18,8 +18,6 @@ describe DynFork::Fields::ColorField do
       f.required?.should be_false
       f.unique?.should be_false
       f.ignored?.should be_false
-      f.maxlength.should eq(256)
-      f.minlength.should eq(0)
       f.hint.should eq("")
       f.warning.should eq(["Examples: #fff | #f2f2f2 | #f2f2f200 | rgb(255,0,24) | rgba(255,0,24,0.5) | rgba(#fff,0.5) | hsl(120,100%,50%) | hsla(170,23%,25%,0.2) | 0x00ffff"])
       f.errors.should eq(Array(String).new)
